6e07b7701ad65fddcbe408b9ba06e0f4edb7e0db
Chris Boddy <christoph.boddy@gmail.com>
2014-04-23 17:35:04 +0100

    fixed bokred merge.

--- peergos/tests/Tests.java
+++ peergos/tests/Tests.java
@@ -10,17 +10,11 @@ public class Tests
 
     public static void main(String[] args)
     {
-<<<<<<< HEAD
-        //testClass(Components.class);
-        //testClass(Crypto.class);
-        testClass(CoreNode.class);
-=======
 //        testClass(Components.class);
 //        testClass(ErasureCodes.class);
         testClass(GaloisPolynomial.Test.class);
 //        testClass(Crypto.class);
 //        testClass(CoreNode.class);
->>>>>>> 96fa7beb9c093c6262fd40c4dc51f83c00270fbd
     }
 
     private static void testClass(Class c)
