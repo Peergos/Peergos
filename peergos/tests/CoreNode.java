1c09815b236c785b825ec966a5a89dee8e616e6a
Ian Preston <ianopolous@gmail.com>
2014-04-29 14:57:22 +0100

    made the fragments in a chunk upload in parallel, now chunk upload takes under 5 minutes, still too high!

--- peergos/tests/CoreNode.java
+++ peergos/tests/CoreNode.java
@@ -3,6 +3,7 @@ package peergos.tests;
 
 import static org.junit.Assert.*;
 
+import akka.actor.ActorSystem;
 import peergos.crypto.*;
 import peergos.corenode.*;
 import peergos.user.UserContext;
@@ -25,74 +26,78 @@ public class CoreNode
         public void close(){};
     }
 
-    public void coreNodeTests(AbstractCoreNode coreNode) throws Exception
-    {
-        User user = User.random();
-        String username = "USER";
-        UserContext context = new UserContext(username, user, null, coreNode);
-        assertTrue("Checking for username", !context.checkRegistered());
-        assertTrue("added user", context.register());
+    public void coreNodeTests(AbstractCoreNode coreNode) throws Exception {
+        ActorSystem system = null;
+        try {
+            system = ActorSystem.create("UserRouter");
+            User user = User.random();
+            String username = "USER";
+            UserContext context = new UserContext(username, user, null, coreNode, system);
+            assertTrue("Checking for username", !context.checkRegistered());
+            assertTrue("added user", context.register());
 
-        //
-        //add sharing key to this user
-        //
-        User follower = User.random();
-        assertTrue("follow user", context.addSharingKey(follower.getKey()));
+            //
+            //add sharing key to this user
+            //
+            User follower = User.random();
+            assertTrue("follow user", context.addSharingKey(follower.getKey()));
 
-        //
-        //retrieve sharing key
-        //
-        boolean retrievedSharingKey = false;
-        Iterator<UserPublicKey> sharingKeys = coreNode.getSharingKeys(username);
-        while (sharingKeys.hasNext()) {
-            UserPublicKey next = sharingKeys.next();
-            if (Arrays.equals(next.getKey().getEncoded(), follower.getKey().getEncoded()))
-                retrievedSharingKey = true;
+            //
+            //retrieve sharing key
+            //
+            boolean retrievedSharingKey = false;
+            Iterator<UserPublicKey> sharingKeys = coreNode.getSharingKeys(username);
+            while (sharingKeys.hasNext()) {
+                UserPublicKey next = sharingKeys.next();
+                if (Arrays.equals(next.getKey().getEncoded(), follower.getKey().getEncoded()))
+                    retrievedSharingKey = true;
+            }
+            assertTrue("retrieved sharing key ", retrievedSharingKey);
+
+            //generate some test data
+            //
+            byte[] fragmentData = new byte[500];
+            random.nextBytes(fragmentData);
+            byte[] cipherText = follower.encryptMessageFor(fragmentData);
+
+            //
+            //add fragment
+            //
+            byte[] mapKey = new byte[10];
+            boolean addedFragment = coreNode.addFragment(username, follower.getPublicKey(), mapKey, cipherText, follower.hashAndSignMessage(cipherText));
+            assertTrue("added fragment", !addedFragment);
+
+            // add storage allowance
+
+            int frags = 10;
+            for (int i = 0; i < frags; i++) {
+                coreNode.registerFragment(username, new InetSocketAddress("localhost", 666), new byte[10 + i]);
+            }
+            long quota = coreNode.getQuota(username);
+            assertTrue("quota after registering fragment", quota == coreNode.fragmentLength() * frags);
+
+            // try again adding fragment
+            addedFragment = coreNode.addFragment(username, follower.getPublicKey(), mapKey, cipherText, follower.hashAndSignMessage(cipherText));
+            assertTrue("added fragment", addedFragment);
+
+            // get fragment and verify contents are the same
+            ByteArrayWrapper blob = coreNode.getFragment(username, follower.getPublicKey(), mapKey);
+            assertTrue("retrieved blob equality", new ByteArrayWrapper(cipherText).equals(blob));
+
+            //
+            //create a friend
+            //
+            User friend = User.random();
+            String friendname = "FRIEND";
+            UserContext friendContext = new UserContext(friendname, friend, null, coreNode, system);
+
+            //
+            //add to coreNode
+            //
+            assertTrue("added friend", friendContext.register());
+        } finally {
+            system.shutdown();
         }
-        assertTrue("retrieved sharing key ", retrievedSharingKey);
-
-        //generate some test data
-        //
-        byte[] fragmentData = new byte[500];
-        random.nextBytes(fragmentData);
-        byte[] cipherText = follower.encryptMessageFor(fragmentData);
-
-        //
-        //add fragment
-        //
-        byte[] mapKey = new byte[10];
-        boolean addedFragment = coreNode.addFragment(username, follower.getPublicKey(), mapKey, cipherText, follower.hashAndSignMessage(cipherText));
-        assertTrue("added fragment", !addedFragment);
-
-        // add storage allowance
-
-        int frags = 10;
-        for (int i=0; i < frags; i++) {
-            coreNode.registerFragment(username, new InetSocketAddress("localhost", 666), new byte[10+i]);
-        }
-        long quota = coreNode.getQuota(username);
-        assertTrue("quota after registering fragment", quota == coreNode.fragmentLength()*frags);
-
-        // try again adding fragment
-        addedFragment = coreNode.addFragment(username, follower.getPublicKey(), mapKey, cipherText, follower.hashAndSignMessage(cipherText));
-        assertTrue("added fragment", addedFragment);
-
-        // get fragment and verify contents are the same
-        ByteArrayWrapper blob = coreNode.getFragment(username, follower.getPublicKey(), mapKey);
-        assertTrue("retrieved blob equality", new ByteArrayWrapper(cipherText).equals(blob));
-
-        //
-        //create a friend
-        //
-        User friend = User.random();
-        String friendname = "FRIEND";
-        UserContext friendContext = new UserContext(friendname, friend, null, coreNode);
-
-        //
-        //add to coreNode
-        //
-        assertTrue("added friend", friendContext.register());
-
     }
 
     @Test public void abstractTest() throws Exception 
