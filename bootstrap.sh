259e4b6dc3c009c20ebe50bc7c11bdfd89106a40
Ian Preston <ianopolous@gmail.com>
2014-10-09 08:58:35 +0000

    bootstrap fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,21 +3,21 @@ cp DirectoryCertificates.src peergos/crypto/DirectoryCertificates.java
 cp CoreCertificates.src peergos/crypto/CoreCertificates.java
 echo generating root certificate...
 java -jar PeergosServer.jar -rootGen -password password
-make server > /dev/null
 cp peergos/crypto/RootCertificate.java RootCertificate.src
+make server > /dev/null
 echo generating directory certificate...
 domain=peergos.org
 java -jar PeergosServer.jar -dirGen -password password -keyfile dir.key -domain $domain
 echo signing directory certificate...
 java -jar PeergosServer.jar -dirSign -csr dir.csr -rootPassword password
-make server > /dev/null
 cp peergos/crypto/DirectoryCertificates.java DirectoryCertificates.src
+make server > /dev/null
 echo generating core node certificate...
 java -jar PeergosServer.jar -coreGen -password password -keyfile core.key -domain $domain
 echo signing core node certificate...
 java -jar PeergosServer.jar -coreSign -csr core.csr -rootPassword password
-make server > /dev/null
 cp peergos/crypto/CoreCertificates.java CoreCertificates.src
+make server > /dev/null
 #
 # To run unit tests successfully, re-make tests after running this script
 #
