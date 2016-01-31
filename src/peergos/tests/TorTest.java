8e253abdff1133d1bcadb8f7b6d87da4d755c036
Ian Preston <ianopolous@gmail.com>
2016-01-31 13:05:05 +0000

    Added tor test, though connection doesn't work yet

---
+++ src/peergos/tests/TorTest.java
@@ -0,0 +1,41 @@
+package peergos.tests;
+
+import org.junit.Test;
+import com.subgraph.orchid.*;
+
+import java.io.*;
+import java.net.*;
+import javax.net.*;
+
+public class TorTest {
+
+    @Test
+    public void connect() throws IOException {
+        TorClient tor = new TorClient();
+        tor.start();
+        while (true)
+            try {
+                tor.waitUntilReady();
+                break;
+            } catch (InterruptedException e) {}
+
+        SocketFactory sf = tor.getSocketFactory();
+
+        URL url = new URL("https://demo.peergos.net");
+        String websiteAddress = url.getHost();
+
+        String file = url.getFile();
+        Socket clientSocket = sf.createSocket(websiteAddress, 80);
+        BufferedReader response = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        OutputStreamWriter outWriter = new OutputStreamWriter(clientSocket.getOutputStream());
+        outWriter.write("GET " + file + " HTTP/1.0\r\n\n");
+        outWriter.flush();
+
+        StringBuilder b = new StringBuilder();
+        String line;
+        while ((line = response.readLine()) != null)
+            b.append(line + "\n");
+        String resp = b.toString();
+        System.out.println(resp);
+    }
+}
