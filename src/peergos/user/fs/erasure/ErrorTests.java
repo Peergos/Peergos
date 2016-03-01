30cea9fb0ed03c9719aac891a5579be85735eb65
Ian Preston <ianopolous@gmail.com>
2016-03-01 22:45:08 +0000

    Fun with binomials. (Erasure failure probability calculation)

--- src/peergos/user/fs/erasure/ErrorTests.java
+++ src/peergos/user/fs/erasure/ErrorTests.java
@@ -2,38 +2,93 @@ package peergos.user.fs.erasure;
 
 import org.junit.*;
 
+import java.math.*;
 import java.util.*;
+import java.util.function.*;
 import java.util.stream.*;
 
 public class ErrorTests {
-    byte[] original;
-    int fragments = 40, maxErrors = 10;
+    public static class ErasureParameters {
+        @org.junit.Test
+        public void findAcceptableErasureParameters() {
+            int totalUserSizeInMB = 10000;
+            int nChunks = totalUserSizeInMB / 5;
+            Predicate<BigDecimal> acceptableFailure = pr -> pr.doubleValue() < 1.0/nChunks; // Expect not to lose a single chunk
+            Stream.of(60).forEach(n -> IntStream.range(1, 6).map(i -> i*n/6)
+                    .forEach(k -> Stream.of(0.5, 0.7, 0.8, 0.9).forEach(p -> {
+                        BigDecimal prFail = probabilityFailure(n, k, p);
+                        if (acceptableFailure.test(prFail))
+                            System.out.printf("%d, %d, %.2f -> %.15f\n", n, k, p, prFail);
+                    }
+                    )
+                    )
+            );
 
-    public ErrorTests() {}
+            double p = 0.7;
+            int f = 40, e = 30;
+            int n = f + 2*e, k = f + e;
+            while (!acceptableFailure.test(probabilityFailure(n, k, p)))
+                p = (1 + p)/2;
+            System.out.printf("%d, %d, %.2f -> %.15f\n", n, k, p, probabilityFailure(n, k, p));
+        }
+    }
 
-    @Before
-    public void init() {
-        original = new byte[5 * 1024 * 1024];
-        new Random().nextBytes(original);
+    /*
+    *  The probability of data loss using a k of n erasure code with each fragment having pr(correct) = p
+    * */
+    public static BigDecimal probabilityFailure(int n, int k, double p) {
+        List<BigDecimal> collect = IntStream.range(0, k)
+                .mapToObj(i -> new BigDecimal(p).pow(i).multiply(new BigDecimal(1 - p).pow(n - i)).multiply(choose(n, i))).collect(Collectors.toList());
+        return collect.stream()
+        .reduce(new BigDecimal(0), (a, b) -> a.add(b));
+    }
+
+    static BigInteger[][] choose = new BigInteger[200][200];
+    static {
+        for (int i=0; i < choose.length; i++) {
+            choose[i][0] = BigInteger.valueOf(1);
+            choose[i][i] = BigInteger.valueOf(1);
+        }
+        for (int i=1; i < choose.length; i++)
+            for (int j=1; j < i; j++)
+                choose[i][j] = choose[i-1][j-1].add(choose[i-1][j]);
+    }
+
+    private static BigDecimal choose(int n, int k) {
+        return new BigDecimal(choose[n][k]);
     }
 
     @Test
-    public void recoverFromerrors() {
-        IntStream.range(0, maxErrors+1).forEach(this::recover);
+    public void recoverFromErrors() {
+        // 40 -> 128 KiB fragments which is nice
+        recoverFromerrors(40, 30);
+    }
+
+    @Test
+    public void standardRecovery() {
+        recoverFromerrors(40, 10);
+    }
+
+    public void recoverFromerrors(int fragments, int maxErrors) {
+        // this is a fragments + maxErrors of fragments + 2*maxErrors erasure scheme
+        byte[] original = new byte[5 * 1024 * 1024];
+        new Random().nextBytes(original);
+
+        IntStream.range(0, maxErrors+1).forEach(e -> recover(original, fragments, maxErrors, e));
         IntStream.range(maxErrors + 1, 2*maxErrors).forEach(e ->
         {
             try {
-                recover(e);
-                throw new RuntimeException("Should fail wih this many errors!");
+                recover(original, fragments, maxErrors, e);
+                throw new RuntimeException("Should fail with this many errors!");
             } catch (IllegalStateException err) {}
         }
         );
     }
 
-    public void recover(int errors) {
+    public void recover(byte[] original, int fragments, int maxErrors, int actualErrors) {
         byte[][] encoded = Erasure.split(original, fragments, maxErrors);
         Set<Integer> done = new HashSet<>();
-        while (errors - done.size() > 0) {
+        while (actualErrors - done.size() > 0) {
             int index = new Random().nextInt(encoded.length);
             if (!done.contains(index)) {
                 done.add(index);
@@ -42,7 +97,7 @@ public class ErrorTests {
         }
         byte[] recovered = Erasure.recombine(encoded, 5 * 1024 * 1024, fragments, maxErrors);
         if (!Arrays.equals(original, recovered))
-            throw new IllegalStateException("Different result from original with "+errors+" errors!");
+            throw new IllegalStateException("Different result from original with "+actualErrors+" errors!");
         System.out.println();
     }
 }
