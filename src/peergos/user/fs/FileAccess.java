dccd4bc42c07ebfc449709b5f3b6a6d4b7736236
Chris <chris@boddy.im>
2016-03-09 21:54:59 +0000

    WIP Added more of the implementation of RetrievedFilePointer and FileAccess  based on api.js

--- src/peergos/user/fs/FileAccess.java
+++ src/peergos/user/fs/FileAccess.java
@@ -1,7 +1,9 @@
 package peergos.user.fs;
 
 import org.bouncycastle.util.Arrays;
+import peergos.crypto.SymmetricLink;
 import peergos.crypto.symmetric.SymmetricKey;
+import peergos.user.UserContext;
 import peergos.util.ByteArrayWrapper;
 import peergos.util.Serialize;
 
@@ -10,11 +12,49 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class FileAccess {
-    public static class EncryptedFileProperties{
-        //todo : wrap byte array
+    public enum Type{FILE, DIRECTORY};
+
+    public final SymmetricLink parent2Meta;
+    public final FileProperties fileProperties;
+    public final FileRetriever fileRetriever;
+    public final SymmetricLocationLink parentLink;
+    public final Type type;
+
+    public FileAccess(SymmetricLink parent2Meta, FileProperties fileProperties, FileRetriever fileRetriever, SymmetricLocationLink parentLink, Type type) {
+        this.parent2Meta = parent2Meta;
+        this.fileProperties = fileProperties;
+        this.fileRetriever = fileRetriever;
+        this.parentLink = parentLink;
+        this.type = type;
     }
+
+    public void serialize(DataOutputStream dout) throws IOException {
+        dout.write(parent2Meta.serialize());
+        dout.write(fileProperties.serialize());
+        fileRetriever.serialize(dout);
+        parentLink.serialize(dout);
+        dout.writeUTF(type.name());
+    }
+
+    public boolean isDirectory() {
+        return type == Type.DIRECTORY;
+    }
+
+    public SymmetricKey getMetaKey(SymmetricKey parentKey) {
+        return parent2Meta.target(parentKey);
+    }
+
+    public boolean removeFragments(UserContext userContext) {
+        throw new IllegalStateException("Deprecated");
+    }
+
+
+
     public static class  FileRetriever{
         //todo
+        public void serialize(DataOutputStream dout) throws IOException {
+            throw new IllegalStateException("Unimplemented");
+        }
     }
 
 
--- src/peergos/user/fs/RetrievedFilePointer.java
+++ src/peergos/user/fs/RetrievedFilePointer.java
@@ -1,8 +1,7 @@
 package peergos.user.fs;
 
-/**
- * Created by chrirs on 09/03/16.
- */
+import peergos.user.UserContext;
+
 public class RetrievedFilePointer {
     public final ReadableFilePointer readableFilePointer;
     public final FileAccess fileAccess;
@@ -10,5 +9,28 @@ public class RetrievedFilePointer {
     public RetrievedFilePointer(ReadableFilePointer readableFilePointer, FileAccess fileAccess) {
         this.readableFilePointer = readableFilePointer;
         this.fileAccess = fileAccess;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RetrievedFilePointer that = (RetrievedFilePointer) o;
+
+        return readableFilePointer != null ? readableFilePointer.equals(that.readableFilePointer) : that.readableFilePointer == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return readableFilePointer != null ? readableFilePointer.hashCode() : 0;
+    }
+    
+    public boolean remove(UserContext userContext, RetrievedFilePointer parent) {
+        if (! readableFilePointer.isWritable())
+            return false;
+        // TODO: 09/03/16
+        return true;
     }
 }
