e35165ef4e512e52b4f8d74072fe33add0b141e5
Chris <chris@boddy.im>
2016-04-20 12:03:29 +0100

    Added separate test cases to FuseTests for moving, copying, truncating, setting the last modified time of files and creating and removing directores.
    Note: some of these tests fail!

--- src/peergos/tests/FuseTests.java
+++ src/peergos/tests/FuseTests.java
@@ -1,6 +1,9 @@
 package peergos.tests;
 
 import org.junit.*;
+import static org.junit.Assert.*;
+import static java.util.UUID.*;
+
 import peergos.corenode.*;
 import peergos.crypto.*;
 import peergos.fuse.*;
@@ -13,6 +16,8 @@ import java.lang.*;
 import java.lang.reflect.*;
 import java.net.*;
 import java.nio.file.*;
+import java.nio.file.attribute.FileTime;
+import java.time.ZonedDateTime;
 import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.*;
@@ -24,6 +29,7 @@ public class FuseTests {
     public static String password = username;
     public static Path mountPoint, home;
     public static FuseProcess fuseProcess;
+    public static Random RANDOM = new Random(666);
 
     public static void setWebPort(int webPort) {
         WEB_PORT = webPort;
@@ -54,7 +60,8 @@ public class FuseTests {
     @BeforeClass
     public static void init() throws Exception {
         Random  random  = new Random();
-        int offset = random.nextInt(100);
+//        int offset = random.nextInt(100);
+        int offset = 0;
         setWebPort(8888 + offset);
         setCorePort(7777 + offset);
 
@@ -90,11 +97,146 @@ public class FuseTests {
         return new String(Serialize.readFully(p.getInputStream())).trim();
     }
 
+    @Test public void  createFileTest() throws IOException  {
+        Path resolve = home.resolve(UUID.randomUUID().toString());
+        assertFalse("file already exists", resolve.toFile().exists());
+        resolve.toFile().createNewFile();
+        assertTrue("file exists after creation", resolve.toFile().exists());
+    }
+
+    @Test public void moveTest() throws IOException {
+        Path initial = createRandomFile(0x1000);
+
+        byte[] initialData = Files.readAllBytes(initial);
+
+        Path target = initial.getParent().resolve(randomUUID().toString());
+        assertFalse("target exists", target.toFile().exists());
+
+        Files.move(initial, target);
+
+        assertFalse("initial still exists", initial.toFile().exists());
+        assertTrue("target exists", target.toFile().exists());
+        byte[] targetData = Files.readAllBytes(target);
+
+        assertTrue("target contents equal to iniital contents", Arrays.equals(initialData, targetData));
+    }
+
+    @Test public void copyFileTest() throws IOException  {
+        Path initial = createRandomFile(0x1000);
+        Path target = initial.getParent().resolve(randomUUID().toString());
+
+        assertFalse("target exists", target.toFile().exists());
+        Files.copy(initial, target);
+
+        assertTrue("initial exists", initial.toFile().exists());
+        assertTrue("target exists", target.toFile().exists());
+
+        boolean contentEquals = Arrays.equals(
+                Files.readAllBytes(initial),
+                Files.readAllBytes(target));
+
+        assertTrue("initial and target contents equal", contentEquals);
+    }
+
+    @Test public  void removeTest() throws IOException {
+        Path path = createRandomFile();
+        assertTrue("path exists", path.toFile().exists());
+        Files.delete(path);
+        assertFalse("path exists", path.toFile().exists());
+    }
+
+    @Test public  void truncateTest() throws IOException {
+        int initialLength = 0x1000;
+        Path path = createRandomFile(initialLength);
+
+        try (RandomAccessFile raf = new RandomAccessFile(path.toFile(), "rw")) {
+            assertEquals("initial size", initialLength, raf.length());
+
+            for (int pow = -1; pow < 4; pow++) {
+                int newSize = (int) (Math.pow(2, pow) * initialLength);
+                raf.setLength(newSize);
+                assertEquals("truncated size equals", newSize, raf.length());
+            }
+        }
+    }
+
+    @Test public  void lastModifiedTimeTest() throws IOException {
+        Path path = createRandomFile();
+        FileTime lastModifiedTime = Files.getLastModifiedTime(path);
+        long epochMillis = lastModifiedTime.toMillis();
+        System.out.println("last modified time " + lastModifiedTime +" "+ new Date(epochMillis));
+
+        long currentTimeMillis = System.currentTimeMillis();
+        long delta = Math.abs(currentTimeMillis - epochMillis);
+        int tolerance = 1000*60*3;
+//        assertTrue("last modified time up to date", delta < tolerance);
+
+        long yesterdayEpochMillis = ZonedDateTime.now().minusDays(1).toInstant().toEpochMilli();
+        FileTime updatedTimestamp = FileTime.fromMillis(yesterdayEpochMillis);
+
+        Files.setLastModifiedTime(path, updatedTimestamp);
+        FileTime updatedLastModifiedTime = Files.getLastModifiedTime(path);
+
+        assertEquals("last-modified  timestamp is set", updatedTimestamp, updatedLastModifiedTime);
+    }
+
+
+
+    @Test public void mkdirsTest() throws IOException {
+
+        String[] stem = Stream.generate(randomUUID()::toString)
+                .limit(10)
+                .toArray(String[]::new);
+
+        Path path = Paths.get(home.toString(), stem);
+        assertFalse("path exists initially", path.toFile().exists());
+
+        path.toFile().mkdirs();
+
+        assertTrue("path is directory", path.toFile().isDirectory());
+    }
+
+    @Test public  void rmdirTest() throws IOException {
+        Path path = home.resolve(randomUUID().toString());
+
+        assertFalse("dir exists initially",
+                path.toFile().exists());
+
+        path.toFile().mkdir();
+
+        assertTrue("dir exists after creation",
+                path.toFile().exists());
+
+        path.toFile().delete();
+
+        assertFalse("dir exists after deletion",
+                path.toFile().exists());
+    }
+
+
+    private Path createRandomFile() throws IOException {
+        return createRandomFile(0);
+    }
+
+    private Path createRandomFile(int length) throws IOException {
+        Path resolve = home.resolve(UUID.randomUUID().toString());
+        resolve.toFile().createNewFile();
+
+        if (length > 0) {
+            byte[] data =  new byte[length];
+            RANDOM.nextBytes(data);
+            Files.write(resolve, data);
+        }
+
+        return resolve;
+    }
+
+
     @Test
     public void variousTests() throws IOException {
         boolean homeExists = Stream.of(mountPoint.toFile().listFiles())
                 .map(f -> f.getName())
-                .filter(n -> n.equals(username))
+                .filter(username::equals)
                 .findAny()
                 .isPresent();
         Assert.assertTrue("Correct home directory: " + homeExists, homeExists);
@@ -110,15 +252,17 @@ public class FuseTests {
         byte[] smallFileContents = Files.readAllBytes(filename1);
         Assert.assertTrue("Correct file contents: " + msg, Arrays.equals(smallFileContents, msg.getBytes()));
 
-
         // rename a file
         Path newFileName = home.resolve("moredata.txt");
-        boolean updatedFileAlreadyExists = newFileName.toFile().exists();
-        Assert.assertFalse("updated file "+  newFileName+" doesn't already exist", updatedFileAlreadyExists);
+        Supplier<Boolean> newPathExists = () -> newFileName.toFile().exists();
+        Assert.assertFalse("updated file "+  newFileName+" doesn't already exist", newPathExists.get());
 
         Files.move(filename1, newFileName);
         byte[] movedContents  = Files.readAllBytes(newFileName);
 
+        Assert.assertFalse("original file "+ filename1 +" present after move", filename1.toFile().exists());
+
+        Assert.assertTrue("updated file "+  newFileName+" exist", newPathExists.get());
         Assert.assertTrue("Correct moved file contents", Arrays.equals(movedContents, msg.getBytes()));
 
         // mkdir
@@ -140,6 +284,8 @@ public class FuseTests {
 
         Assert.assertTrue("new file in directory "+ inDir +" exist", inDirExists.get());
 
+        Assert.assertFalse("previous file in directory "+ newFileName +" present after move op", newPathExists.get());
+
         byte[] inDirContents =  Files.readAllBytes(inDir);
 
         Assert.assertTrue("Correct file contents after move to another directory", Arrays.equals(inDirContents, msg.getBytes()));
@@ -149,7 +295,7 @@ public class FuseTests {
         byte[] data = new byte[length];
         random.nextBytes(data);
 
-        String filename = UserTests.randomString();
+        String filename = randomUUID().toString();
         Path path = home.resolve(filename);
 
         Files.write(path, data);
