5dabe9486fbe2dc673b231678d20f6bf30eb84e4
Ian Preston <ianopolous@gmail.com>
2016-07-07 23:21:58 +0100

    touch to trigger travis

--- src/peergos/user/fs/FileTreeNode.java
+++ src/peergos/user/fs/FileTreeNode.java
@@ -252,7 +252,7 @@ public class FileTreeNode {
                 context.random.randombytes(mapKey, 0, 32);
                 Location nextChunkLocation = nextChunkLocationOpt.orElse(new Location(getLocation().owner, getLocation().writer, mapKey));
 
-                System.out.println("********** Writing to chunk at mapkey: "+ArrayOps.bytesToHex(currentOriginal.location.mapKey) + " next: "+nextChunkLocation);
+                System.out.println("********* Writing to chunk at mapkey: "+ArrayOps.bytesToHex(currentOriginal.location.mapKey) + " next: "+nextChunkLocation);
                 // modify chunk, re-encrypt and upload
                 int internalStart = (int) (startIndex % Chunk.MAX_SIZE);
                 int internalEnd = endIndex - (startIndex - internalStart) > Chunk.MAX_SIZE ?
