e9cb735aad23fe2ffca03cfafa9431ccd1b00990
Ian Preston <ianopolous@gmail.com>
2016-11-16 22:50:26 +0000

    Add UserGenerationAlgorithm to WriterData (not used anywhere yet)

--- src/peergos/shared/storage/ContentAddressedStorage.java
+++ src/peergos/shared/storage/ContentAddressedStorage.java
@@ -9,12 +9,20 @@ import java.util.stream.*;
 public interface ContentAddressedStorage {
 
     int MAX_OBJECT_LENGTH  = 1024*256;
+
     /**
      *
      * @param object
      * @return a hash of the stored object
      */
     Multihash put(MerkleNode object);
+
+    /**
+     *
+     * @param key the hash of a value previously stored
+     * @return
+     */
+    byte[] get(Multihash key);
 
     default Multihash put(byte[] data, List<Multihash> links) {
         return put(new MerkleNode(data, links.stream().collect(Collectors.toMap(m -> m.toString(), m -> m))));
@@ -23,13 +31,6 @@ public interface ContentAddressedStorage {
     default Multihash put(byte[] data) {
         return put(new MerkleNode(data, Collections.emptyMap()));
     }
-
-    /**
-     *
-     * @param key the hash of a value previously stored
-     * @return
-     */
-    byte[] get(Multihash key);
 
     /**
      *
