c60a05a701256a59ad3b048e82065057212f29b2
Ian Preston <ianopolous@gmail.com>
2016-12-14 20:07:56 +0000

    added test

--- src/peergos/server/tests/MerkleNodeSerialization.java
+++ src/peergos/server/tests/MerkleNodeSerialization.java
@@ -9,8 +9,20 @@ import java.util.*;
 public class MerkleNodeSerialization {
 
     @Test
-    public void inverses() throws IOException {
+    public void emptyNode() throws IOException {
         MerkleNode original = new MerkleNode(new byte[0], Collections.emptyList());
+        testMerkleNode(original);
+    }
+
+    @Test
+    public void nonEmpty() throws IOException {
+        byte[] data = new byte[100];
+        new Random(1).nextBytes(data);
+        MerkleNode original = new MerkleNode(data, Collections.emptyList());
+        testMerkleNode(original);
+    }
+
+    private void testMerkleNode(MerkleNode original) throws IOException {
 
         byte[] cbor = original.serialize();
 
@@ -23,4 +35,5 @@ public class MerkleNodeSerialization {
         Assert.assertTrue("Same deserialization", equal);
         Assert.assertTrue("Same serialization", sameCbor);
     }
+
 }
