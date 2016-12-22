55b456e585588a3d3b7eb97dda97d81088e1eadc
Ian Preston <ianopolous@gmail.com>
2016-12-22 23:10:22 +0000

    Implement a general CBOR encoding to generalise object <=> cbor

---
+++ src/peergos/server/tests/CborObjects.java
@@ -0,0 +1,74 @@
+package peergos.server.tests;
+
+import org.junit.*;
+import peergos.shared.cbor.*;
+
+import java.util.*;
+
+public class CborObjects {
+    private final Random rnd = new Random();
+
+    private byte[] random(int len) {
+        byte[] res = new byte[len];
+        rnd.nextBytes(res);
+        return res;
+    }
+
+    @Test
+    public void cborNull() {
+        CborObject.CborNull cbor = new CborObject.CborNull();
+        compatibleAndIdempotentSerialization(cbor);
+    }
+
+    @Test
+    public void cborString() {
+        String value = "G'day mate!";
+        CborObject.CborString cbor = new CborObject.CborString(value);
+        compatibleAndIdempotentSerialization(cbor);
+    }
+
+    @Test
+    public void cborByteArray() {
+        byte[] value = random(32);
+        CborObject.CborByteArray cbor = new CborObject.CborByteArray(value);
+        compatibleAndIdempotentSerialization(cbor);
+    }
+
+    @Test
+    public void cborLongs() {
+        cborLong(rnd.nextLong());
+        cborLong(Long.MAX_VALUE);
+        cborLong(Long.MIN_VALUE);
+        cborLong(Integer.MAX_VALUE);
+        cborLong(Integer.MIN_VALUE);
+        cborLong(0);
+        cborLong(100);
+        cborLong(-100);
+    }
+
+    private void cborLong(long value) {
+        CborObject.CborLong cbor = new CborObject.CborLong(value);
+        compatibleAndIdempotentSerialization(cbor);
+    }
+
+    @Test
+    public void cborMap() {
+        SortedMap<CborObject, CborObject> map = new TreeMap<>();
+        map.put(new CborObject.CborString("KEY 1"), new CborObject.CborString("A value"));
+        map.put(new CborObject.CborString("KEY 2"), new CborObject.CborByteArray("A value".getBytes()));
+        map.put(new CborObject.CborString("KEY 3"), new CborObject.CborNull());
+        CborObject.CborMap cborMap = new CborObject.CborMap(map);
+        compatibleAndIdempotentSerialization(cborMap);
+    }
+
+    public void compatibleAndIdempotentSerialization(CborObject value) {
+        byte[] raw = value.toByteArray();
+        CborObject deserialized = CborObject.fromByteArray(raw);
+
+        boolean equals = deserialized.equals(value);
+        Assert.assertTrue("Equal objects", equals);
+        byte[] raw2 = deserialized.toByteArray();
+        boolean sameRaw = Arrays.equals(raw, raw2);
+        Assert.assertTrue("Idempotent serialization", sameRaw);
+    }
+}
---
+++ src/peergos/shared/cbor/CborObject.java
@@ -0,0 +1,239 @@
+package peergos.shared.cbor;
+
+import peergos.shared.util.*;
+
+import java.io.*;
+import java.util.*;
+
+public interface CborObject {
+
+    void serialize(CborEncoder encoder);
+
+    default byte[] toByteArray() {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        CborEncoder encoder = new CborEncoder(bout);
+        serialize(encoder);
+        return bout.toByteArray();
+    }
+
+    static CborObject fromByteArray(byte[] cbor) {
+        return deserialize(new CborDecoder(new ByteArrayInputStream(cbor)));
+    }
+
+    static CborObject deserialize(CborDecoder decoder) {
+        try {
+            CborType type = decoder.peekType();
+            switch (type.getMajorType()) {
+                case CborConstants.TYPE_TEXT_STRING:
+                    return new CborString(decoder.readTextString());
+                case CborConstants.TYPE_BYTE_STRING:
+                    return new CborByteArray(decoder.readByteString());
+                case CborConstants.TYPE_UNSIGNED_INTEGER:
+                    return new CborLong(decoder.readInt());
+                case CborConstants.TYPE_NEGATIVE_INTEGER:
+                    return new CborLong(decoder.readInt());
+                case CborConstants.TYPE_FLOAT_SIMPLE:
+                    if (type.getAdditionalInfo() == CborConstants.NULL)
+                        return new CborNull();
+                    throw new IllegalStateException("Unimplemented simple type!");
+                case CborConstants.TYPE_MAP: {
+                    long nValues = decoder.readMapLength();
+                    SortedMap<CborObject, CborObject> result = new TreeMap<>();
+                    for (long i=0; i < nValues; i++) {
+                        CborObject key = deserialize(decoder);
+                        CborObject value = deserialize(decoder);
+                        result.put(key, value);
+                    }
+                    return new CborMap(result);
+                }
+                default:
+                    throw new IllegalStateException("Unimplemented cbor type: " + type);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    final class CborMap implements CborObject {
+        public final SortedMap<CborObject, CborObject> values;
+
+        public CborMap(SortedMap<CborObject, CborObject> values) {
+            this.values = values;
+        }
+
+        @Override
+        public void serialize(CborEncoder encoder) {
+            try {
+                encoder.writeMapStart(values.size());
+                for (Map.Entry<CborObject, CborObject> entry : values.entrySet()) {
+                    entry.getKey().serialize(encoder);
+                    entry.getValue().serialize(encoder);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            CborMap cborMap = (CborMap) o;
+
+            return values != null ? values.equals(cborMap.values) : cborMap.values == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            return values != null ? values.hashCode() : 0;
+        }
+    }
+
+    final class CborByteArray implements CborObject, Comparable<CborByteArray> {
+        private final byte[] value;
+
+        public CborByteArray(byte[] value) {
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(CborByteArray other) {
+            return ArrayOps.compare(value, other.value);
+        }
+
+        @Override
+        public void serialize(CborEncoder encoder) {
+            try {
+                encoder.writeByteString(value);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            CborByteArray that = (CborByteArray) o;
+
+            return Arrays.equals(value, that.value);
+
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(value);
+        }
+    }
+
+    final class CborString implements CborObject, Comparable<CborString> {
+
+        public final String value;
+
+        public CborString(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(CborString cborString) {
+            return value.compareTo(cborString.value);
+        }
+
+        @Override
+        public void serialize(CborEncoder encoder) {
+            try {
+                encoder.writeTextString(value);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            CborString that = (CborString) o;
+
+            return value.equals(that.value);
+
+        }
+
+        @Override
+        public int hashCode() {
+            return value.hashCode();
+        }
+    }
+
+    final class CborLong implements CborObject, Comparable<CborLong> {
+        public final long value;
+
+        public CborLong(long value) {
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(CborLong other) {
+            return Long.compare(value, other.value);
+        }
+
+        @Override
+        public void serialize(CborEncoder encoder) {
+            try {
+                encoder.writeInt(value);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            CborLong cborLong = (CborLong) o;
+
+            return value == cborLong.value;
+
+        }
+
+        @Override
+        public int hashCode() {
+            return (int) (value ^ (value >>> 32));
+        }
+    }
+
+    final class CborNull implements CborObject, Comparable<CborNull> {
+        public CborNull() {}
+
+        @Override
+        public int compareTo(CborNull cborNull) {
+            return 0;
+        }
+
+        @Override
+        public void serialize(CborEncoder encoder) {
+            try {
+                encoder.writeNull();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+    }
+}
