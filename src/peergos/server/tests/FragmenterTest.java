888c125c971f0a886565f67c96e0593d060d06fa
Ian Preston <ianopolous@gmail.com>
2017-01-24 20:24:59 +0000

    move test into server package for gwt

---
+++ src/peergos/server/tests/FragmenterTest.java
@@ -0,0 +1,79 @@
+package peergos.server.tests;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
+import peergos.shared.user.fs.*;
+import peergos.shared.util.*;
+
+import java.io.*;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class FragmenterTest {
+    private static Random random = new Random(666);
+
+    private final Fragmenter fragmenter;
+
+    public FragmenterTest(Fragmenter fragmenter) {
+        this.fragmenter = fragmenter;
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> parameters() {
+        return Arrays.asList(new Object[][]{
+                {new SplitFragmenter()},
+                {new ErasureFragmenter(EncryptedChunk.ERASURE_ORIGINAL, EncryptedChunk.ERASURE_ALLOWED_FAILURES)}
+        });
+    }
+
+    @Test
+    public void testSeries() throws IOException {
+        for (int i = 1; i < 10; i++) {
+            int length = random.nextInt(Chunk.MAX_SIZE);
+            byte[] b = new byte[length];
+            test(b);
+        }
+    }
+
+    @Test
+    public void testBoundary() throws IOException {
+        List<Integer> sizes = Arrays.asList(Fragment.MAX_LENGTH, 2 * Fragment.MAX_LENGTH);
+        for (Integer size : sizes) {
+            byte[] b = new byte[size];
+            test(b);
+        }
+    }
+
+    private void test(byte[] input) throws IOException {
+        random.nextBytes(input);
+
+
+        byte[][] split = fragmenter.split(input);
+
+        for (byte[] bytes : split) {
+            int length = bytes.length;
+            assertTrue(length > 0);
+            assertTrue(length <= Fragment.MAX_LENGTH);
+        }
+
+        byte[] recombine = fragmenter.recombine(split, input.length);
+
+        assertTrue("recombine(split(input)) = input", Arrays.equals(input, recombine));
+    }
+
+
+    @Test
+    public void serializationTest() throws IOException {
+        DataSink sink = new DataSink();
+        fragmenter.serialize(sink);
+
+        Fragmenter deserialize = Fragmenter.deserialize(new DataInputStream(
+                new ByteArrayInputStream(sink.toByteArray())));
+
+        assertEquals(fragmenter, deserialize);
+    }
+}
