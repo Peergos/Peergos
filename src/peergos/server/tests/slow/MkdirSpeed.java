a407d4dd60f0ea3ca5295068165fb12895515253
Ian Preston <ianopolous@gmail.com>
2017-04-14 22:36:33 +0100

    Added different mkdir profiling configurations which suggest that Multipart is the main user of time and allocator of memory!

--- src/peergos/server/tests/slow/MkdirSpeed.java
+++ src/peergos/server/tests/slow/MkdirSpeed.java
@@ -4,8 +4,12 @@ import org.junit.*;
 import org.junit.runner.*;
 import org.junit.runners.*;
 import peergos.server.*;
+import peergos.server.corenode.*;
+import peergos.server.storage.*;
 import peergos.shared.*;
+import peergos.shared.corenode.*;
 import peergos.shared.crypto.*;
+import peergos.shared.storage.*;
 import peergos.shared.user.*;
 import peergos.shared.user.fs.*;
 import peergos.shared.util.*;
@@ -18,28 +22,42 @@ import java.util.stream.*;
 @RunWith(Parameterized.class)
 public class MkdirSpeed {
 
-    public static int RANDOM_SEED = 666;
+    private static int RANDOM_SEED = 666;
     private final NetworkAccess network;
     private final Crypto crypto = Crypto.initJava();
 
     private static Random random = new Random(RANDOM_SEED);
 
-    public MkdirSpeed(String useIPFS, Random r) throws Exception {
+    public MkdirSpeed(boolean useHttp, String useIPFS, Random r) throws Exception {
+        this.network = useHttp ? buildInProcessAccess(r) : buildHttpNetworkAccess(useIPFS.equals("IPFS"), r);
+
+        // use insecure random otherwise tests take ages
+        setFinalStatic(TweetNaCl.class.getDeclaredField("prng"), new Random(1));
+    }
+
+    private static NetworkAccess buildInProcessAccess(Random r) throws Exception {
+        CoreNode core = SQLiteCoreNode.build(":memory:");
+        ContentAddressedStorage dht = new RAMStorage();
+        Btree btree = new BtreeImpl(core, dht);
+        return new NetworkAccess(core, dht, btree, Collections.emptyList());
+    }
+
+    private static NetworkAccess buildHttpNetworkAccess(boolean useIpfs, Random r) throws Exception {
         int portMin = 9000;
         int portRange = 2000;
         int webPort = portMin + r.nextInt(portRange);
         int corePort = portMin + portRange + r.nextInt(portRange);
-        Args args = Args.parse(new String[]{"useIPFS", ""+useIPFS.equals("IPFS"), "-port", Integer.toString(webPort), "-corenodePort", Integer.toString(corePort)});
+        Args args = Args.parse(new String[]{"useIPFS", "" + useIpfs, "-port", Integer.toString(webPort), "-corenodePort", Integer.toString(corePort)});
         Start.local(args);
-        this.network = NetworkAccess.buildJava(new URL("http://localhost:" + webPort)).get();
-        // use insecure random otherwise tests take ages
-        setFinalStatic(TweetNaCl.class.getDeclaredField("prng"), new Random(1));
+        return NetworkAccess.buildJava(new URL("http://localhost:" + webPort)).get();
     }
 
     @Parameterized.Parameters()
     public static Collection<Object[]> parameters() {
         return Arrays.asList(new Object[][] {
-                {"IPFS", new Random(0)}
+//                {false, "IPFS", new Random(0)}
+                {false, "NOTIPFS", new Random(0)}
+//                {true, "IPFS", new Random(0)}
         });
     }
 
@@ -61,6 +79,10 @@ public class MkdirSpeed {
         return UserContext.ensureSignedUp(username, password, network, crypto).get();
     }
 
+    // (0 - 100 mkdirs)
+    // All ram, not http =>  40 -   54 ms
+    // All ram, http     => 400 -  416 ms
+    // IPFS, http        => 660 -  840 ms
     @Test
     public void hugeFolder() throws Exception {
         String username = generateUsername();
@@ -68,7 +90,7 @@ public class MkdirSpeed {
         UserContext context = ensureSignedUp(username, password, network, crypto);
         FileTreeNode userRoot = context.getUserRoot().get();
         List<String> names = new ArrayList<>();
-        IntStream.range(0, 1000).forEach(i -> names.add(randomString()));
+        IntStream.range(0, 100).forEach(i -> names.add(randomString()));
 
         long worst = 0, best = Long.MAX_VALUE, start = System.currentTimeMillis();
         for (int i=0; i < names.size(); i++) {
