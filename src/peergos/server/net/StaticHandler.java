bfc40a0e329fbede220fb6a47e339a44af42969a
Ian Preston <ianopolous@gmail.com>
2017-09-28 21:04:43 +0100

    add all http headers except one that broke the service worker

--- src/peergos/server/net/StaticHandler.java
+++ src/peergos/server/net/StaticHandler.java
@@ -67,7 +67,7 @@ public abstract class StaticHandler implements HttpHandler
             }
 
             // Only allow assets to be loaded from the original host
-            httpExchange.getResponseHeaders().set("content-security-policy", "default-src https: 'self'");
+//            httpExchange.getResponseHeaders().set("content-security-policy", "default-src https: 'self'");
             // Don't anyone to load Peergos site in an iframe
             httpExchange.getResponseHeaders().set("x-frame-options", "DENY");
             // Enable cross site scripting protection
