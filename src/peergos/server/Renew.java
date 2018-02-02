4b8677c567cc5d20d1777008b79c60ef75e2eb2c
Ian Preston <ianopolous@gmail.com>
2018-02-02 18:54:20 +0000

    add a renew username utility

---
+++ src/peergos/server/Renew.java
@@ -0,0 +1,23 @@
+package peergos.server;
+
+import peergos.shared.*;
+import peergos.shared.user.*;
+
+import java.io.*;
+import java.net.*;
+import java.time.*;
+
+public class Renew {
+
+    public static void main(String[] args) throws Exception {
+        Crypto crypto = Crypto.initJava();
+        NetworkAccess network = NetworkAccess.buildJava(new URL("https://demo.peergos.net")).get();
+        String username = args[0];
+        LocalDate expiry = LocalDate.parse(args[1]);
+        Console console = System.console();
+        String password = new String(console.readPassword("Enter password for " + username + ":"));
+        UserContext context = UserContext.signIn(username, password, network, crypto).get();
+        context.renewUsernameClaim(expiry).get();
+        System.out.println("Logged in " + username + " successfully!");
+    }
+}
