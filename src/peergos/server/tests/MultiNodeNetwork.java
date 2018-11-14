43eaa236028c9ba47634e458a9c0ea731d89dd19
Dr Ian Preston <ianopolous@protonmail.com>
2018-11-14 18:37:48 +0000

    Users signed up on different peergos servers can friend each other!!!

--- src/peergos/server/tests/MultiNodeNetwork.java
+++ src/peergos/server/tests/MultiNodeNetwork.java
@@ -4,6 +4,7 @@ import org.junit.*;
 import peergos.server.*;
 import peergos.server.util.*;
 import peergos.shared.*;
+import peergos.shared.crypto.symmetric.*;
 import peergos.shared.io.ipfs.multihash.*;
 import peergos.shared.user.*;
 import peergos.shared.user.fs.*;
@@ -11,6 +12,7 @@ import peergos.shared.util.*;
 
 import java.net.*;
 import java.util.*;
+import java.util.stream.*;
 
 import static org.junit.Assert.*;
 
@@ -60,6 +62,37 @@ public class MultiNodeNetwork {
     @Test
     public void signUpOnNormalNode() throws Exception {
         UserContext context = ensureSignedUp(generateUsername(), randomString(), nodes.get(1), crypto);
+    }
+
+    @Test
+    public void internodeFriends() throws Exception {
+        String username1 = generateUsername();
+        String password1 = randomString();
+        UserContext u1 = ensureSignedUp(username1, password1, nodes.get(1), crypto);
+        String username2 = generateUsername();
+        String password2 = randomString();
+        UserContext u2 = ensureSignedUp(username2, password2, nodes.get(0), crypto);
+
+        u2.sendFollowRequest(username1, SymmetricKey.random()).get();
+        List<FollowRequest> u1Requests = u1.processFollowRequests().get();
+        assertTrue("Receive a follow request", u1Requests.size() > 0);
+        u1.sendReplyFollowRequest(u1Requests.get(0), true, true).get();
+        List<FollowRequest> u2FollowRequests = u2.processFollowRequests().get();
+        Optional<FileTreeNode> u1ToU2 = u2.getByPath("/" + u1.username).get();
+        assertTrue("Friend root present after accepted follow request", u1ToU2.isPresent());
+
+        Optional<FileTreeNode> u2ToU1 = u1.getByPath("/" + u2.username).get();
+        assertTrue("Friend root present after accepted follow request", u2ToU1.isPresent());
+
+        Set<String> u1Following = ensureSignedUp(username1, password1, nodes.get(1).clear(), crypto).getSocialState().get()
+                .followingRoots.stream().map(f -> f.getName())
+                .collect(Collectors.toSet());
+        assertTrue("Following correct", u1Following.contains(u2.username));
+
+        Set<String> u2Following = ensureSignedUp(username2, password2, nodes.get(0).clear(), crypto).getSocialState().get()
+                .followingRoots.stream().map(f -> f.getName())
+                .collect(Collectors.toSet());
+        assertTrue("Following correct", u2Following.contains(u1.username));
     }
 
     private String generateUsername() {
