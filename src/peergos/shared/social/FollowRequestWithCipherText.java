8b79293de002a87baeafb000b7997384f31ff97d
Dr Ian Preston <ianopolous@protonmail.com>
2019-01-03 14:44:09 +1100

    Add method for JS to get entrypoint

--- src/peergos/shared/social/FollowRequestWithCipherText.java
+++ src/peergos/shared/social/FollowRequestWithCipherText.java
@@ -1,5 +1,8 @@
 package peergos.shared.social;
 
+import jsinterop.annotations.*;
+import peergos.shared.user.*;
+
 public class FollowRequestWithCipherText {
 
     public final FollowRequest req;
@@ -8,5 +11,10 @@ public class FollowRequestWithCipherText {
     public FollowRequestWithCipherText(FollowRequest req, BlindFollowRequest cipher) {
         this.req = req;
         this.cipher = cipher;
+    }
+
+    @JsMethod
+    public EntryPoint getEntry() {
+        return req.entry.get();
     }
 }
