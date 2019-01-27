337b77b6a4dce6faa1fdaf8ed25a84214d92905b
Dr Ian Preston <ianopolous@protonmail.com>
2019-01-27 20:48:37 +0000

    Improve social network javadoc

--- src/peergos/shared/social/SocialNetwork.java
+++ src/peergos/shared/social/SocialNetwork.java
@@ -10,24 +10,25 @@ public interface SocialNetwork {
 
     /** Send a follow request to the target public key
      *
-     * @param target
-     * @param encryptedPermission
-     * @return
+     * @param target The public identity key hash of the target user
+     * @param encryptedPermission The encrypted follow request
+     * @return True if successful
      */
     CompletableFuture<Boolean> sendFollowRequest(PublicKeyHash target, byte[] encryptedPermission);
 
     /**
      *
-     * @param owner
-     * @return all the pending follow requests for the given public key
+     * @param owner The public identity key hash of user who's pending follow requests are being retrieved
+     * @param signedTime The current time signed by the owner
+     * @return all the pending follow requests for the given user
      */
     CompletableFuture<byte[]> getFollowRequests(PublicKeyHash owner, byte[] signedTime);
 
     /** Delete a follow request for a given public key
      *
-     * @param owner
-     * @param data
-     * @return
+     * @param owner The public identity key hash of user who's follow request is being deleted
+     * @param data The original follow request data to delete, signed by the owner
+     * @return True if successful
      */
     CompletableFuture<Boolean> removeFollowRequest(PublicKeyHash owner, byte[] data);
 }
