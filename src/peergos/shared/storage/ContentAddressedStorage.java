11aa6f331dd44740da80b81d2d824ffa133213e5
Dr Ian Preston <ianopolous@protonmail.com>
2019-01-27 19:27:43 +0000

    Javadoc ContentAddressedStorage

--- src/peergos/shared/storage/ContentAddressedStorage.java
+++ src/peergos/shared/storage/ContentAddressedStorage.java
@@ -55,26 +55,97 @@ public interface ContentAddressedStorage {
      */
     CompletableFuture<Multihash> id();
 
+    /**
+     *
+     * @param owner
+     * @return A new transaction id that can be used to group writes together and protect them from being garbage
+     * collected before they have been pinned.
+     */
     CompletableFuture<TransactionId> startTransaction(PublicKeyHash owner);
 
+    /**
+     * Release all associated objects from this transaction to allow them to be garbage collected if they haven't been
+     * pinned.
+     * @param owner
+     * @param tid
+     * @return
+     */
     CompletableFuture<Boolean> closeTransaction(PublicKeyHash owner, TransactionId tid);
 
+    /**
+     *
+     * @param owner The owner of these blocks of data
+     * @param writer The public signing key authorizing these writes, which must be owned by the owner key
+     * @param signatures The signatures of each block being written (by the writer)
+     * @param blocks The blocks to write
+     * @param tid The transaction to group these writes under
+     * @return
+     */
     CompletableFuture<List<Multihash>> put(PublicKeyHash owner, PublicKeyHash writer, List<byte[]> signatures, List<byte[]> blocks, TransactionId tid);
 
-    CompletableFuture<Optional<CborObject>> get(Multihash object);
+    /**
+     *
+     * @param hash
+     * @return The data with the requested hash, deserialized into cbor, or Optional.empty() if no object can be found
+     */
+    CompletableFuture<Optional<CborObject>> get(Multihash hash);
 
+    /**
+     * Write a block of data that is just raw bytes, not ipld structured cbor
+     * @param owner
+     * @param writer
+     * @param signatures
+     * @param blocks
+     * @param tid
+     * @return
+     */
     CompletableFuture<List<Multihash>> putRaw(PublicKeyHash owner, PublicKeyHash writer, List<byte[]> signatures, List<byte[]> blocks, TransactionId tid);
 
-    CompletableFuture<Optional<byte[]>> getRaw(Multihash object);
+    /**
+     * Get a block of data that is not in ipld cbor format, just raw bytes
+     * @param hash
+     * @return
+     */
+    CompletableFuture<Optional<byte[]>> getRaw(Multihash hash);
 
+    /**
+     * Update an existing pin with a new root. This is useful when modifying a tree of ipld objects where only a small
+     * number of components are changed
+     * @param owner The owner of the data
+     * @param existing The present root hash
+     * @param updated The new root hash
+     * @return
+     */
     CompletableFuture<List<MultiAddress>> pinUpdate(PublicKeyHash owner, Multihash existing, Multihash updated);
 
-    CompletableFuture<List<Multihash>> recursivePin(PublicKeyHash owner, Multihash h);
+    /**
+     * Recursively pin all the objects referenced via ipld merkle links from a root object
+     * @param owner The owner of the data
+     * @param hash The root hash of the merkle-tree
+     * @return A list of the multihashes pinned
+     */
+    CompletableFuture<List<Multihash>> recursivePin(PublicKeyHash owner, Multihash hash);
 
-    CompletableFuture<List<Multihash>> recursiveUnpin(PublicKeyHash owner, Multihash h);
+    /**
+     * Recursively unpin a merkle tree of objects. This releases the objects to be collected by garbage collection
+     * @param owner The owner of the data
+     * @param hash The root hash of the merkle-tree
+     * @return
+     */
+    CompletableFuture<List<Multihash>> recursiveUnpin(PublicKeyHash owner, Multihash hash);
 
+    /**
+     * Get all the merkle-links referenced directly from this object
+     * @param root The hash of the object whose links we want
+     * @return A list of the multihashes referenced with ipld links in this object
+     */
     CompletableFuture<List<Multihash>> getLinks(Multihash root);
 
+    /**
+     * Get the size in bytes of the object with the requested hash
+     * @param block The hash of the object
+     * @return The size in bytes, or Optional.empty() if it cannot be found.
+     */
     CompletableFuture<Optional<Integer>> getSize(Multihash block);
 
     default CompletableFuture<PublicKeyHash> putSigningKey(byte[] signature,
