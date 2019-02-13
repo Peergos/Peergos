583e41d238109ae1b6bdcf3ef3e4b4a57ec394d4
Dr Ian Preston <ianopolous@protonmail.com>
2019-02-13 20:26:10 +0000

    javadoc

--- src/peergos/shared/storage/controller/InstanceAdmin.java
+++ src/peergos/shared/storage/controller/InstanceAdmin.java
@@ -7,6 +7,16 @@ import peergos.shared.util.*;
 import java.util.*;
 import java.util.concurrent.*;
 
+/**
+ * This is how the administrator of a Peergos instance can control it including:
+ *
+ * 1) Upgrade the Peergos version
+ * 2) Change local user quotas
+ * 3) Change policy on allowing new user sign ups
+ * 4) Grant/revoke admin rights to another user
+ * 5) Add/remove ipfs instances to the storage cluster
+ * 6) Change the erasure code/raid policy for data storage per user
+ */
 public interface InstanceAdmin {
 
     CompletableFuture<VersionInfo> getVersionInfo();
