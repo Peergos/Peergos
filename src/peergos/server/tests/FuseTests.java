bba9fa45a2fc071e3b1c56757cf8f49122b65a1c
Jonathan Budd <jonathan.budd@protonmail.com>
2019-04-07 20:38:44 +0100

    Added getCause to error logging.

--- src/peergos/server/tests/FuseTests.java
+++ src/peergos/server/tests/FuseTests.java
@@ -75,7 +75,7 @@ public class FuseTests {
             Files.createTempFile(resolve, "", "");
         }
         catch(FileSystemException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getCause().getMessage());
         }
         assertTrue("file exists after creation", resolve.toFile().exists());
     }
