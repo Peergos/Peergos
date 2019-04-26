893f71c63b157992642ab9e95a1f158399ef26e9
Dr Ian Preston <ianopolous@protonmail.com>
2019-04-26 14:37:27 +0100

    Add login benchmark

---
+++ src/peergos/server/tests/slow/LoginBenchmark.java
@@ -0,0 +1,70 @@
+package peergos.server.tests.slow;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
+import peergos.server.*;
+import peergos.server.tests.*;
+import peergos.server.util.*;
+import peergos.shared.*;
+import peergos.shared.user.*;
+
+import java.net.*;
+import java.util.*;
+
+@RunWith(Parameterized.class)
+public class LoginBenchmark {
+
+    private static int RANDOM_SEED = 666;
+    private final NetworkAccess network;
+    private final Crypto crypto = Crypto.initJava();
+
+    private static Random random = new Random(RANDOM_SEED);
+
+    public LoginBenchmark(String useIPFS, Random r) throws Exception {
+        this.network = buildHttpNetworkAccess(useIPFS.equals("IPFS"), r);
+    }
+
+    private static NetworkAccess buildHttpNetworkAccess(boolean useIpfs, Random r) throws Exception {
+        Args args = UserTests.buildArgs().with("useIPFS", "" + useIpfs);
+        Main.PKI_INIT.main(args);
+        return NetworkAccess.buildJava(new URL("http://localhost:" + args.getInt("port"))).get();
+    }
+
+    @Parameterized.Parameters()
+    public static Collection<Object[]> parameters() {
+        return Arrays.asList(new Object[][] {
+//                {"IPFS", new Random(0)}
+                {"NOTIPFS", new Random(0)}
+        });
+    }
+
+    private String generateUsername() {
+        return "test" + (random.nextInt() % 10000);
+    }
+
+    public static UserContext ensureSignedUp(String username, String password, NetworkAccess network, Crypto crypto) throws Exception {
+        return UserContext.ensureSignedUp(username, password, network, crypto).get();
+    }
+
+    // LOGIN(93) duration: 3083 mS, best: 2744 mS, worst: 3409 mS, av: 2858 mS
+    //    scrypt: 1429 mS
+    //    pointers.get: 27*45ms = 1300 mS This should only be 2 calls which would halve login time
+    @Test
+    public void login() throws Exception {
+        String username = generateUsername();
+        String password = "test01";
+        ensureSignedUp(username, password, network, crypto);
+
+        long worst = 0, best = Long.MAX_VALUE, start = System.currentTimeMillis();
+        for (int i=0; i < 20; i++) {
+            long t1 = System.currentTimeMillis();
+            ensureSignedUp(username, password, network, crypto);
+            long duration = System.currentTimeMillis() - t1;
+            worst = Math.max(worst, duration);
+            best = Math.min(best, duration);
+            System.err.printf("LOGIN(%d) duration: %d mS, best: %d mS, worst: %d mS, av: %d mS\n", i,
+                    duration, best, worst, (t1 + duration - start) / (i + 1));
+        }
+    }
+}
