8cbd8793026d251508399ee0b390b60e368fe9d8
Dr Ian Preston <ianopolous@protonmail.com>
2019-06-17 20:19:59 +0100

    Fix error messages

--- src/peergos/server/util/TimeLimited.java
+++ src/peergos/server/util/TimeLimited.java
@@ -13,11 +13,11 @@ public class TimeLimited {
         try {
             Optional<PublicSigningKey> ownerOpt = ipfs.getSigningKey(owner).get();
             if (! ownerOpt.isPresent())
-                throw new IllegalStateException("Couldn't retrieve owner key during getFollowRequests() call!");
+                throw new IllegalStateException("Couldn't retrieve owner key!");
             byte[] raw = ownerOpt.get().unsignMessage(signedTime);
             CborObject cbor = CborObject.fromByteArray(raw);
             if (! (cbor instanceof CborObject.CborLong))
-                throw new IllegalStateException("Invalid cbor for getFollowRequests authorisation!");
+                throw new IllegalStateException("Invalid cbor for time in authorisation!");
             long utcMillis = ((CborObject.CborLong) cbor).value;
             long now = System.currentTimeMillis();
             if (Math.abs(now - utcMillis) > durationSeconds * 1_000)
