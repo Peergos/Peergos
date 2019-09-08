4e61c65cd5467c450d0b506eca406cfe51c094e0
Chris Boddy <chris@boddy.im>
2019-09-08 19:55:05 +0100

    [native-image] standalone binary distribution
    Added script for building Peergos as a standalone binary with the
    graalvm/native-image tool.
    
    Also since it's quite abstruse another script for  installing that tool.

---
+++ build_native_image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# Compiles ./PeergosServer.jar into a standalone binary using native-image.
+# More details @i https://www.graalvm.org/docs/reference-manual/aot-compilation/
+# 
+# notes:
+#   PeergosServer.jar is built separately with the ant script
+#   native-image is required 
+#   
+# usage:
+# ./build_native_image.sh <peergos-jar-path> <native-image-path>
+#  
+
+PEERGOS_JAR_PATH=${1:-PeergosServer.jar}
+NATIVE_IMAGE_BIN=${NATIVE_IMAGE_BIN:-native-image}
+if ! type -P $NATIVE_IMAGE_BIN;
+then
+    echo "Could not find native-image executable! Run ./install-native-image.sh and add it to \$PATH"
+    exit 1
+fi
+
+# no-fallback is required
+$NATIVE_IMAGE_BIN -cp "lib/*" -jar "$PEERGOS_JAR_PATH" --no-fallback
+
+    
+
---
+++ scripts/install-native-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# to build a native-image graalvm-ce is required
+# and then native-image is installed in a separate step
+# note: make sure the graalvm version is up to date
+#
+#
+
+curl  -L -O https://github.com/oracle/graal/releases/download/vm-19.2.0/graalvm-ce-linux-amd64-19.2.0.tar.gz
+tar -zxvf graalvm-ce-linux-amd64-19.2.0.tar.gz
+./graalvm-ce-19.2.0/bin/gu install native-image
+
+if [ -f  "graalvm-ce-19.2.0/bin/native-image" ];
+then
+    echo "native-image installed @ "$(readlink -f graalvm-ce-19.2.0/bin/native-image)
+    export native-image=$(readlink -f graalvm-ce-19.2.0/bin/native-image)
+else
+    echo "native-image not installed..."
+fi
+
+
