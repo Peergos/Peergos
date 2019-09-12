096e409e2ff520bf9b3f9047e4c00aea7dbae013
Dr Ian Preston <ianopolous@protonmail.com>
2019-09-12 14:44:44 +0100

    Native image works! and executable runs correctly!!!
    I manually added the require reflection and jni classes by reading the
    C source of the version of sqlite we are using.
    
    Becuase of this I've commented the reflection run step for now.

--- build_native_image.sh
+++ build_native_image.sh
@@ -6,6 +6,7 @@
 # 
 # notes:
 #   PeergosServer.jar is built separately with the ant script
+#   zlib is required: sudo apt install zlib1g-dev
 #   native-image is required 
 #   
 # usage:
@@ -22,9 +23,9 @@ then
 fi
 
 # run peergos first to log required reflected class names
-mkdir -p META-INF/native-image
-$NATIVE_IMAGE_JAVA -agentlib:native-image-agent=config-output-dir=META-INF/native-image -jar PeergosServer.jar -peergos -default-quota 209715200 -useIPFS true -peergos.identity.hash z59vuwzfFDp3ZA8ZpnnmHEuMtyA1q34m3Th49DYXQVJntWpxdGrRqXi -pki-node-id QmVdFZgHnEgcedCS2G2ZNiEN59LuVrnRm7z3yXtEBv2XiF -max-users 2
+#mkdir -p META-INF/native-image
+#$NATIVE_IMAGE_JAVA -agentlib:native-image-agent=config-output-dir=META-INF/native-image -jar PeergosServer.jar -peergos -default-quota 209715200 -useIPFS true -peergos.identity.hash z59vuwzfFDp3ZA8ZpnnmHEuMtyA1q34m3Th49DYXQVJntWpxdGrRqXi -pki-node-id QmVdFZgHnEgcedCS2G2ZNiEN59LuVrnRm7z3yXtEBv2XiF -max-users 2
 # no-fallback is required
-$NATIVE_IMAGE_BIN -H:EnableURLProtocols=http -H:+ReportUnsupportedElementsAtRuntime -H:ConfigurationFileDirectories=META-INF/native-image --no-server --no-fallback --initialize-at-build-time=org.sqlite.DB,org.sqlite.NativeDB -jar "$PEERGOS_JAR_PATH" peergos
+$NATIVE_IMAGE_BIN -H:EnableURLProtocols=http -H:+ReportUnsupportedElementsAtRuntime -H:ConfigurationFileDirectories=META-INF/native-image --no-server --no-fallback --initialize-at-build-time=org.sqlite.DB,org.sqlite.NativeDB,org.sqlite.Function,org.sqlite.Function\$Aggregate,org.sqlite.DB\$ProgressObserver -jar "$PEERGOS_JAR_PATH" peergos
     
 
