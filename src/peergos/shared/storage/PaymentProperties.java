163a540dce8a364b7126645a5d305c54e8547c35
ian <ianopolous@protonmail.com>
2020-03-28 17:17:23 +0000

    Add error to payment properties

--- src/peergos/shared/storage/PaymentProperties.java
+++ src/peergos/shared/storage/PaymentProperties.java
@@ -9,27 +9,43 @@ public final class PaymentProperties  implements Cborable {
 
     public final Optional<String> paymentServerUrl;
     public final Optional<String> clientSecret;
+    public final Optional<String> error;
     public final long freeQuota;
     public final long desiredQuota;
 
-    private PaymentProperties(Optional<String> paymentServerUrl, Optional<String> clientSecret, long freeQuota, long desiredQuota) {
+    private PaymentProperties(Optional<String> paymentServerUrl,
+                              Optional<String> error,
+                              Optional<String> clientSecret,
+                              long freeQuota,
+                              long desiredQuota) {
         this.paymentServerUrl = paymentServerUrl;
+        this.error = error;
         this.clientSecret = clientSecret;
         this.freeQuota = freeQuota;
         this.desiredQuota = desiredQuota;
     }
 
     public PaymentProperties(long freeQuota) {
-        this(Optional.empty(), Optional.empty(), freeQuota, 0);
+        this(Optional.empty(), Optional.empty(), Optional.empty(), freeQuota, 0);
     }
 
     public PaymentProperties(String paymentServerUrl, Optional<String> clientSecret, long freeQuota, long desiredQuota) {
-        this(Optional.of(paymentServerUrl), clientSecret, freeQuota, desiredQuota);
+        this(Optional.of(paymentServerUrl), Optional.empty(), clientSecret, freeQuota, desiredQuota);
     }
 
     @JsMethod
     public boolean isPaid() {
         return paymentServerUrl.isPresent();
+    }
+
+    @JsMethod
+    public boolean hasError() {
+        return error.isPresent();
+    }
+
+    @JsMethod
+    public String getError() {
+        return error.get();
     }
 
     @JsMethod
@@ -42,12 +58,21 @@ public final class PaymentProperties  implements Cborable {
         return clientSecret.orElse("");
     }
 
+    public static PaymentProperties errored(String paymentServerUrl,
+                                            String error,
+                                            Optional<String> clientSecret,
+                                            long freeQuota,
+                                            long desiredQuota) {
+        return new PaymentProperties(Optional.of(paymentServerUrl), Optional.of(error), clientSecret, freeQuota, desiredQuota);
+    }
+
     @Override
     public CborObject toCbor() {
         SortedMap<String, Cborable> state = new TreeMap<>();
         state.put("freeQuota", new CborObject.CborLong(desiredQuota));
         state.put("desiredQuota", new CborObject.CborLong(desiredQuota));
         paymentServerUrl.ifPresent(url -> state.put("url", new CborObject.CborString(url)));
+        error.ifPresent(err -> state.put("err", new CborObject.CborString(err)));
         if (clientSecret.isPresent())
             state.put("client_secret", new CborObject.CborString(clientSecret.get()));
         return CborObject.CborMap.build(state);
@@ -58,9 +83,10 @@ public final class PaymentProperties  implements Cborable {
             throw new IllegalStateException("Invalid cbor for FileProperties! " + cbor);
         CborObject.CborMap m = (CborObject.CborMap) cbor;
         Optional<String> url = m.getOptional("url", c -> ((CborObject.CborString)c).value);
+        Optional<String> err = m.getOptional("err", c -> ((CborObject.CborString)c).value);
         Optional<String> client_secret = m.getOptional("client_secret", c -> ((CborObject.CborString) c).value);
         long freeQuota = m.getLong("freeQuota");
         long desiredQuota = m.getLong("desiredQuota");
-        return new PaymentProperties(url, client_secret, freeQuota, desiredQuota);
+        return new PaymentProperties(url, err, client_secret, freeQuota, desiredQuota);
     }
 }
