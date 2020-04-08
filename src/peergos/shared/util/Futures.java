0b571bbf29243c177b689032fa39bf60b5a07f8f
kevin odwyer <kevodwyer@gmail.com>
2020-04-08 12:37:27 +0100

    Make sure to handle exceptions in combine methods

--- src/peergos/shared/util/Futures.java
+++ src/peergos/shared/util/Futures.java
@@ -21,10 +21,19 @@ public class Futures {
      */
     public static <T> CompletableFuture<Set<T>> combineAll(Collection<CompletableFuture<T>> futures) {
         CompletableFuture<Set<T>> identity = CompletableFuture.completedFuture(Collections.emptySet());
+        List<Throwable> inError = new ArrayList();
         return futures.stream().reduce(identity,
-                (a, b) -> b.thenCompose(opt ->
-                        a.thenApply(set -> Stream.concat(set.stream(), Stream.of(opt))
-                                .collect(Collectors.toSet()))),
+                (a, b) -> b.thenCompose(opt -> {
+                        if(! inError.isEmpty()) {
+                            throw new RuntimeException(inError.get(0));
+                        }
+                        return a.thenApply(set -> Stream.concat(set.stream(), Stream.of(opt))
+                                .collect(Collectors.toSet())
+                        ).exceptionally(t -> {
+                            inError.add(t);
+                            throw new RuntimeException(inError.get(0));
+                        });
+                }),
                 (a, b) -> b.thenCompose(setb ->
                         a.thenApply(seta -> Stream.concat(seta.stream(), setb.stream()).collect(Collectors.toSet()))));
     }
@@ -37,16 +46,24 @@ public class Futures {
      */
     public static <T> CompletableFuture<List<T>> combineAllInOrder(Collection<CompletableFuture<T>> futures) {
         CompletableFuture<List<T>> identity = CompletableFuture.completedFuture(Collections.emptyList());
+        List<Throwable> inError = new ArrayList();
         return futures.stream().reduce(identity,
-                (a, b) -> b.thenCompose(opt ->
-                        a.thenApply(set -> {
-                            ArrayList<T> combined = new ArrayList<>(set.size() + 1);
+                (a, b) -> b.thenCompose(opt -> {
+                        if(! inError.isEmpty()) {
+                            throw new RuntimeException(inError.get(0));
+                        }
+                        return a.thenApply(set -> {
+                            List<T> combined = new ArrayList<>(set.size() + 1);
                             combined.addAll(set);
                             combined.add(opt);
                             return combined;
-                        })),
+                        }).exceptionally(t -> {
+                            inError.add(t);
+                            throw new RuntimeException(inError.get(0));
+                        });}),
                 (a, b) -> b.thenCompose(setb ->
-                        a.thenApply(seta -> Stream.concat(seta.stream(), setb.stream()).collect(Collectors.toList()))));
+                        a.thenApply(seta -> Stream.concat(seta.stream(), setb.stream()).collect(Collectors.toList())))
+        );
     }
 
     /*** Reduce a set of input values against an Identity where the composition step is asynchronous
