221df7ac238dbfbbe9b3e2457731863956dd0ce1
kevin odwyer <kevodwyer@gmail.com>
2020-10-14 10:43:30 +0100

    version cbor

--- src/peergos/shared/user/TodoBoard.java
+++ src/peergos/shared/user/TodoBoard.java
@@ -12,6 +12,7 @@ public class TodoBoard implements Cborable {
 
     private final List<TodoList> todoLists;
     private final String name;
+    private static final String VERSION_1 = "1";
 
     private TodoBoard(String name, List<TodoList> todoLists) {
         this.name = name;
@@ -37,6 +38,7 @@ public class TodoBoard implements Cborable {
     @Override
     public CborObject toCbor() {
         Map<String, CborObject> cbor = new TreeMap<>();
+        cbor.put("version", new CborObject.CborString(VERSION_1));
         cbor.put("name", new CborObject.CborString(name));
         cbor.put("lists", new CborObject.CborList(todoLists));
         return CborObject.CborMap.build(cbor);
@@ -46,6 +48,10 @@ public class TodoBoard implements Cborable {
         if (! (cbor instanceof CborObject.CborMap))
             throw new IllegalStateException("TodoList cbor must be a Map! " + cbor);
         CborObject.CborMap m = (CborObject.CborMap) cbor;
+        String version = m.getString("version");
+        if (! version.equals(VERSION_1)) {
+            throw new IllegalStateException("Unsupported version:" + version);
+        }
         String name = m.getString("name");
         List<TodoList> todoList = m.getList("lists")
                 .value.stream()
