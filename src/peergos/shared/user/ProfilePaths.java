620b6814a068f884a59e40e05a134fbbb5fba3a7
ian <ianopolous@protonmail.com>
2020-10-22 12:09:57 +0100

    Implement first few methods for managing profile data

--- src/peergos/shared/user/ProfilePaths.java
+++ src/peergos/shared/user/ProfilePaths.java
@@ -1,6 +1,12 @@
 package peergos.shared.user;
 
+import peergos.shared.user.fs.*;
+import peergos.shared.util.*;
+
 import java.nio.file.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
 
 /** This class stores locations of the different components of a user's profile
  *
@@ -19,4 +25,37 @@ public class ProfilePaths {
     public static final Path EMAIL = ROOT.resolve("email");
     public static final Path WEBROOT = ROOT.resolve("webroot"); // The path in Peergos to this users web root
 
+    private static <V> CompletableFuture<Optional<V>> getAndParse(Path p, Function<byte[], V> parser, UserContext viewer) {
+        return viewer.getByPath(p)
+                .thenCompose(opt -> opt.map(f -> Serialize.readFully(f, viewer.crypto, viewer.network)
+                        .thenApply(parser)
+                        .thenApply(Optional::of))
+                        .orElse(Futures.of(Optional.empty())));
+    }
+
+    private static <V> CompletableFuture<Boolean> serializeAndSet(Path p, V val, Function<V, byte[]> serialize, UserContext user) {
+        byte[] raw = serialize.apply(val);
+        return user.getUserRoot()
+                .thenCompose(home -> home.getOrMkdirs(p.getParent(), user.network, true, user.crypto))
+                .thenCompose(parent -> parent.uploadOrReplaceFile(p.getFileName().toString(),
+                        AsyncReader.build(raw), raw.length, user.network, user.crypto, x -> {},
+                        user.crypto.random.randomBytes(RelativeCapability.MAP_KEY_LENGTH)))
+                .thenApply(x -> true);
+    }
+
+    public static CompletableFuture<Optional<byte[]>> getProfilePhoto(String user, UserContext viewer) {
+        return getAndParse(Paths.get(user).resolve(PHOTO_HIGH_RES), x -> x, viewer);
+    }
+
+    public static CompletableFuture<Boolean> setProfilePhoto(UserContext user, byte[] image) {
+        return serializeAndSet(PHOTO_HIGH_RES, image, x -> x, user);
+    }
+
+    public static CompletableFuture<Optional<String>> getBio(String user, UserContext viewer) {
+        return getAndParse(Paths.get(user).resolve(BIO), String::new, viewer);
+    }
+
+    public static CompletableFuture<Boolean> setBio(UserContext user, String bio) {
+        return serializeAndSet(BIO, bio, String::getBytes, user);
+    }
 }
