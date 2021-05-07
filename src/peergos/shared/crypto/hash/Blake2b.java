f3b1b98a77e443ba6cdc465488b77f83b55c39c6
Jan Domanski <jandom@gmail.com>
2021-05-08 00:43:22 +0100

    Adding further targets and packages

--- src/peergos/shared/crypto/hash/Blake2b.java
+++ src/peergos/shared/crypto/hash/Blake2b.java
@@ -1916,7 +1916,8 @@ public interface Blake2b {
         }
         public final Param setKey (final byte[] key) {
             assert key != null : "key is null";
-            assert key.length >= 0 : assertFail("key.length", key.length, inclusiveUpperBound, 0);
+            // FIXME "Comparison of a size >= 0 is always true, did you intend to check for non-emptiness?"
+            // assert key.length >= 0 : assertFail("key.length", key.length, inclusiveUpperBound, 0);
             assert key.length <= Spec.max_key_bytes : assertFail("key.length", key.length, inclusiveUpperBound, Spec.max_key_bytes);
 
             // zeropad keybytes
