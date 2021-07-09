9bf633cb510757d8447352876219c002993ac201
ian <ianopolous@protonmail.com>
2021-07-09 16:21:53 +0100

    Update last Chat unit test

--- src/peergos/server/tests/MessagingTests.java
+++ src/peergos/server/tests/MessagingTests.java
@@ -141,80 +141,110 @@ public class MessagingTests {
         Assert.assertTrue(stores.get(0).messages.get(5).msg.equals(msg1));
     }
 
-//    @Test
-//    public void partitionAndJoin() {
-//        List<SigningPrivateKeyAndPublicHash> identities = generateUsers(4);
-//        List<PrivateChatState> chatIdentities = generateChatIdentities(4);
-//        List<RamMessageStore> stores = IntStream.range(0, 4).mapToObj(i -> new RamMessageStore()).collect(Collectors.toList());
-//
-//        List<Chat> chats = Chat.createNew(
-//                Arrays.asList("user1", "user2", "user3", "user4"),
-//                identities.stream().map(p -> p.publicKeyHash).collect(Collectors.toList()));
-//        TreeClock genesis = chats.get(0).current;
-//        Chat chat1 = chats.get(0);
-//        Chat chat2 = chats.get(1);
-//        Chat chat3 = chats.get(2);
-//        Chat chat4 = chats.get(3);
-//        OwnerProof user1ChatId = OwnerProof.build(identities.get(0), chatIdentities.get(0).chatIdentity.publicKeyHash);
-//        chat1.join(chat1.host(), user1ChatId, chatIdentities.get(0).chatIdPublic, identities.get(0), stores.get(0), NO_OP, hasher).join();
-//
-//        OwnerProof user2ChatId = OwnerProof.build(identities.get(1), chatIdentities.get(1).chatIdentity.publicKeyHash);
-//        chat2.join(chat2.host(), user2ChatId, chatIdentities.get(1).chatIdPublic, identities.get(1), stores.get(1), NO_OP, hasher).join();
-//
-//        OwnerProof user3ChatId = OwnerProof.build(identities.get(2), chatIdentities.get(2).chatIdentity.publicKeyHash);
-//        chat3.join(chat3.host(), user3ChatId, chatIdentities.get(2).chatIdPublic, identities.get(2), stores.get(2), NO_OP, hasher).join();
-//
-//        OwnerProof user4ChatId = OwnerProof.build(identities.get(3), chatIdentities.get(3).chatIdentity.publicKeyHash);
-//        chat4.join(chat4.host(), user4ChatId, chatIdentities.get(3).chatIdPublic, identities.get(3), stores.get(3), NO_OP, hasher).join();
-//
-//        // partition and chat between user1 and user2
-//        TreeClock t1_0 = chat1.current;
-//        MessageEnvelope msg1 = chat1.sendMessage(text("Hey All, I'm user1!"), chatIdentities.get(0).chatIdentity, stores.get(0), hasher).join();
-//        Assert.assertTrue(msg1.timestamp.isIncrementOf(t1_0));
-//
-//        String chatUid = "chat-uid";
-//        chat2.merge(chatUid, chat1.host, stores.get(0), stores.get(1), ipfs, NO_OP, NO_OP2).join();
-//        TreeClock t2_0 = chat2.current;
-//        MessageEnvelope msg2 = chat2.sendMessage(text("Hey user1! I'm user2."), chatIdentities.get(1).chatIdentity, stores.get(1), hasher).join();
-//        Assert.assertTrue(msg2.timestamp.isIncrementOf(t2_0));
-//
-//        chat1.merge(chatUid, chat2.host, stores.get(1), stores.get(0), ipfs, NO_OP, NO_OP2).join();
-//        TreeClock t1_1 = chat1.current;
-//        MessageEnvelope msg3 = chat1.sendMessage(text("Hey user2, whats up?"), chatIdentities.get(0).chatIdentity, stores.get(0), hasher).join();
-//        Assert.assertTrue(msg3.timestamp.isIncrementOf(t1_1));
-//
-//        chat2.merge(chatUid, chat1.host, stores.get(0), stores.get(1), ipfs, NO_OP, NO_OP2).join();
-//        TreeClock t2_1 = chat2.current;
-//        MessageEnvelope msg4 = chat2.sendMessage(text("Just saving the world one decentralized chat at a time.."),
-//                chatIdentities.get(1).chatIdentity, stores.get(1), hasher).join();
-//        Assert.assertTrue(msg4.timestamp.isIncrementOf(t2_1));
-//
-//        chat1.merge(chatUid, chat2.host, stores.get(1), stores.get(0), ipfs, NO_OP, NO_OP2).join();
-//        Assert.assertTrue(stores.get(1).messages.containsAll(stores.get(0).messages));
-//        Assert.assertEquals(stores.get(1).messages.size(), 6);
-//
-//        // also between user3 and user4
-//        MessageEnvelope msg5 = chat3.sendMessage(text("Hey All, I'm user3!"), chatIdentities.get(2).chatIdentity, stores.get(2), hasher).join();
-//        chat4.merge(chatUid, chat3.host, stores.get(2), stores.get(3), ipfs, NO_OP, NO_OP2).join();
-//        MessageEnvelope msg6 = chat4.sendMessage(text("Hey user3! I'm user4."), chatIdentities.get(3).chatIdentity, stores.get(3), hasher).join();
-//        chat3.merge(chatUid, chat4.host, stores.get(3), stores.get(2), ipfs, NO_OP, NO_OP2).join();
-//        MessageEnvelope msg7 = chat3.sendMessage(text("Hey user4, whats up?"), chatIdentities.get(2).chatIdentity, stores.get(2), hasher).join();
-//        chat4.merge(chatUid, chat3.host, stores.get(2), stores.get(3), ipfs, NO_OP, NO_OP2).join();
-//        MessageEnvelope msg8 = chat4.sendMessage(text("Just saving the world one encrypted chat at a time.."), chatIdentities.get(3).chatIdentity, stores.get(3), hasher).join();
-//        chat3.merge(chatUid, chat4.host, stores.get(3), stores.get(2), ipfs, NO_OP, NO_OP2).join();
-//        Assert.assertTrue(stores.get(3).messages.containsAll(stores.get(2).messages));
-//        Assert.assertEquals(stores.get(3).messages.size(), 6);
-//
-//        // now resolve the partition and merge states
-//        chat1.merge(chatUid, chat4.host, stores.get(3), stores.get(0), ipfs, NO_OP, NO_OP2).join();
-//        Assert.assertEquals(stores.get(0).messages.size(), 12);
-//        chat2.merge(chatUid, chat1.host, stores.get(0), stores.get(1), ipfs, NO_OP, NO_OP2).join();
-//        Assert.assertTrue(stores.get(1).messages.containsAll(stores.get(0).messages));
-//
-//        // check ordering
-//        for (int i=0; i < stores.size(); i++)
-//            validateMessageLog(stores.get(i).getMessagesFrom(0).join(), genesis);
-//    }
+    @Test
+    public void partitionAndJoin() {
+        List<SigningPrivateKeyAndPublicHash> identities = generateUsers(4);
+        List<PrivateChatState> chatIdentities = generateChatIdentities(4);
+        List<RamMessageStore> stores = IntStream.range(0, 4).mapToObj(i -> new RamMessageStore()).collect(Collectors.toList());
+
+        List<Chat> chats = Chat.createNew("uid",
+                Arrays.asList("user1", "user2", "user3", "user4"),
+                identities.stream().map(p -> p.publicKeyHash).collect(Collectors.toList()));
+        TreeClock genesis = chats.get(0).current;
+        Chat chat1 = chats.get(0);
+        Chat chat2 = chats.get(1);
+        Chat chat3 = chats.get(2);
+        Chat chat4 = chats.get(3);
+        OwnerProof user1ChatId = OwnerProof.build(identities.get(0), chatIdentities.get(0).chatIdentity.publicKeyHash);
+        ChatUpdate u1_1 = chat1.join(chat1.host(), user1ChatId, chatIdentities.get(0).chatIdPublic, identities.get(0), stores.get(0), ipfs, hasher).join();
+        stores.get(0).apply(u1_1);
+
+        OwnerProof user2ChatId = OwnerProof.build(identities.get(1), chatIdentities.get(1).chatIdentity.publicKeyHash);
+        ChatUpdate u2_1 = chat2.join(chat2.host(), user2ChatId, chatIdentities.get(1).chatIdPublic, identities.get(1), stores.get(1), ipfs, hasher).join();
+        stores.get(1).apply(u2_1);
+
+        OwnerProof user3ChatId = OwnerProof.build(identities.get(2), chatIdentities.get(2).chatIdentity.publicKeyHash);
+        ChatUpdate u3_1 = chat3.join(chat3.host(), user3ChatId, chatIdentities.get(2).chatIdPublic, identities.get(2), stores.get(2), ipfs, hasher).join();
+        stores.get(2).apply(u3_1);
+
+        OwnerProof user4ChatId = OwnerProof.build(identities.get(3), chatIdentities.get(3).chatIdentity.publicKeyHash);
+        ChatUpdate u4_1 = chat4.join(chat4.host(), user4ChatId, chatIdentities.get(3).chatIdPublic, identities.get(3), stores.get(3), ipfs, hasher).join();
+        stores.get(3).apply(u4_1);
+
+        // partition and chat between user1 and user2
+        TreeClock t1_0 = u1_1.state.current;
+        ChatUpdate u1_2 = u1_1.state.sendMessage(text("Hey All, I'm user1!"), chatIdentities.get(0).chatIdentity, stores.get(0), ipfs, hasher).join();
+        stores.get(0).apply(u1_2);
+        MessageEnvelope msg1 = u1_2.newMessages.get(u1_2.newMessages.size() - 1).msg;
+        Assert.assertTrue(msg1.timestamp.isIncrementOf(t1_0));
+
+        String chatUid = "chat-uid";
+        ChatUpdate u2_2 = u2_1.state.merge(chatUid, chat1.host, stores.get(0), ipfs).join();
+        stores.get(1).apply(u2_2);
+        TreeClock t2_0 = u2_2.state.current;
+        ChatUpdate u2_3 = u2_2.state.sendMessage(text("Hey user1! I'm user2."), chatIdentities.get(1).chatIdentity, stores.get(1), ipfs, hasher).join();
+        stores.get(1).apply(u2_3);
+        MessageEnvelope msg2 = u2_3.newMessages.get(u2_3.newMessages.size() - 1).msg;
+        Assert.assertTrue(msg2.timestamp.isIncrementOf(t2_0));
+
+        ChatUpdate u1_3 = u1_2.state.merge(chatUid, chat2.host, stores.get(1), ipfs).join();
+        stores.get(0).apply(u1_3);
+        TreeClock t1_1 = u1_3.state.current;
+        ChatUpdate u1_4 = u1_3.state.sendMessage(text("Hey user2, whats up?"), chatIdentities.get(0).chatIdentity, stores.get(0), ipfs, hasher).join();
+        stores.get(0).apply(u1_4);
+        MessageEnvelope msg3 = u1_4.newMessages.get(u1_4.newMessages.size() - 1).msg;
+        Assert.assertTrue(msg3.timestamp.isIncrementOf(t1_1));
+
+        ChatUpdate u2_4 = u2_3.state.merge(chatUid, chat1.host, stores.get(0), ipfs).join();
+        stores.get(1).apply(u2_4);
+        TreeClock t2_1 = u2_4.state.current;
+        ChatUpdate u2_5 = u2_4.state.sendMessage(text("Just saving the world one decentralized chat at a time.."),
+                chatIdentities.get(1).chatIdentity, stores.get(1), ipfs, hasher).join();
+        stores.get(1).apply(u2_5);
+        MessageEnvelope msg4 = u2_5.newMessages.get(u2_5.newMessages.size() - 1).msg;
+        Assert.assertTrue(msg4.timestamp.isIncrementOf(t2_1));
+
+        ChatUpdate u1_5 = u1_4.state.merge(chatUid, chat2.host, stores.get(1), ipfs).join();
+        stores.get(0).apply(u1_5);
+        Assert.assertTrue(stores.get(1).messages.containsAll(stores.get(0).messages));
+        Assert.assertEquals(stores.get(1).messages.size(), 6);
+
+        // also between user3 and user4
+        ChatUpdate u3_2 = u3_1.state.sendMessage(text("Hey All, I'm user3!"), chatIdentities.get(2).chatIdentity, stores.get(2), ipfs, hasher).join();
+        stores.get(2).apply(u3_2);
+
+        ChatUpdate u4_2 = u4_1.state.merge(chatUid, chat3.host, stores.get(2), ipfs).join();
+        stores.get(3).apply(u4_2);
+        ChatUpdate u4_3 = u4_2.state.sendMessage(text("Hey user3! I'm user4."), chatIdentities.get(3).chatIdentity, stores.get(3), ipfs, hasher).join();
+        stores.get(3).apply(u4_3);
+
+        ChatUpdate u3_3 = u3_2.state.merge(chatUid, chat4.host, stores.get(3), ipfs).join();
+        stores.get(2).apply(u3_3);
+        ChatUpdate u3_4 = u3_3.state.sendMessage(text("Hey user4, whats up?"), chatIdentities.get(2).chatIdentity, stores.get(2), ipfs, hasher).join();
+        stores.get(2).apply(u3_4);
+
+        ChatUpdate u4_4 = u4_3.state.merge(chatUid, chat3.host, stores.get(2), ipfs).join();
+        stores.get(3).apply(u4_4);
+        ChatUpdate u4_5 = u4_4.state.sendMessage(text("Just saving the world one encrypted chat at a time.."), chatIdentities.get(3).chatIdentity, stores.get(3), ipfs, hasher).join();
+        stores.get(3).apply(u4_5);
+
+        ChatUpdate u3_5 = u3_4.state.merge(chatUid, chat4.host, stores.get(3), ipfs).join();
+        stores.get(2).apply(u3_5);
+        Assert.assertTrue(stores.get(3).messages.containsAll(stores.get(2).messages));
+        Assert.assertEquals(stores.get(3).messages.size(), 6);
+
+        // now resolve the partition and merge states
+        ChatUpdate u1_6 = u1_5.state.merge(chatUid, chat4.host, stores.get(3), ipfs).join();
+        stores.get(0).apply(u1_6);
+        Assert.assertEquals(stores.get(0).messages.size(), 12);
+        ChatUpdate u2_6 = u2_5.state.merge(chatUid, chat1.host, stores.get(0), ipfs).join();
+        stores.get(1).apply(u2_6);
+        Assert.assertTrue(stores.get(1).messages.containsAll(stores.get(0).messages));
+
+        // check ordering
+        for (int i=0; i < stores.size(); i++)
+            validateMessageLog(stores.get(i).getMessagesFrom(0).join(), genesis);
+    }
 
     private static void validateMessageLog(List<SignedMessage> msgs, TreeClock genesis) {
         TreeClock current = genesis;
