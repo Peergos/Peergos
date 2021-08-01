b41973fa36ef98b485d7d9cb021d872e42839717
ian <ianopolous@protonmail.com>
2021-08-01 22:50:57 +0100

    Sanitise paths and use empty path instead of / for getting children
    This fixes copying a shared dir in the UI.
    
    Really we need to type-safe it all and write our own AbsPath and RelPath classes.

--- src/peergos/shared/user/fs/FileWrapper.java
+++ src/peergos/shared/user/fs/FileWrapper.java
@@ -363,13 +363,13 @@ public class FileWrapper {
     @JsMethod
     public CompletableFuture<Set<FileWrapper>> getChildren(Hasher hasher, NetworkAccess network) {
         if (capTrie.isPresent())
-            return capTrie.get().getChildren("/", hasher, network);
+            return capTrie.get().getChildren("", hasher, network);
         return getChildren(version, hasher, network);
     }
 
     public CompletableFuture<Set<FileWrapper>> getChildren(Snapshot version, Hasher hasher, NetworkAccess network) {
         if (capTrie.isPresent())
-            return capTrie.get().getChildren("/", hasher, version.merge(this.version), network);
+            return capTrie.get().getChildren("", hasher, version.merge(this.version), network);
         if (isReadable()) {
             Optional<SigningPrivateKeyAndPublicHash> childsEntryWriter = pointer.capability.wBaseKey
                     .map(wBase -> pointer.fileAccess.getSigner(pointer.capability.rBaseKey, wBase, entryWriter));
