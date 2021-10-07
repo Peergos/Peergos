ec15d5c4f4ebd940325bd8a046afe9ae4520f5e7
ian <ianopolous@protonmail.com>
2021-10-07 17:41:39 +0100

    Restore test parameters

--- src/peergos/server/tests/MultiNodeNetworkTests.java
+++ src/peergos/server/tests/MultiNodeNetworkTests.java
@@ -64,8 +64,8 @@ public class MultiNodeNetworkTests {
     @Parameterized.Parameters(name="nodes: {0}, {1} (0 == PKI, > 0 normal)")
     public static Collection<Object[]> parameters() {
         return Arrays.asList(new Object[][] {
-//                {0, 1}, // PKI, normal-1
-//                {1, 0}, // normal-2, PKI
+                {0, 1}, // PKI, normal-1
+                {1, 0}, // normal-2, PKI
                 {2, 1}  // normal-1, normal-2
         });
     }
