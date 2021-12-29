01c0f2948ce39ec5237022e1e263284dec857a80
ian <ianopolous@protonmail.com>
2021-12-29 03:44:58 +0000

    Fix gwtc
    TODO:
    * upgrade -> list of raw bats

--- src/peergos/server/storage/AuthedCachingStorage.java
+++ src/peergos/server/storage/AuthedCachingStorage.java
@@ -1,10 +1,11 @@
-package peergos.shared.storage;
+package peergos.server.storage;
 
 import peergos.server.storage.auth.*;
 import peergos.shared.cbor.*;
 import peergos.shared.crypto.hash.*;
 import peergos.shared.io.ipfs.cid.*;
 import peergos.shared.io.ipfs.multihash.*;
+import peergos.shared.storage.*;
 import peergos.shared.storage.auth.*;
 import peergos.shared.util.*;
 
