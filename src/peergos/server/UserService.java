11c317f0b38d86543d1f8257051b522a4ce6fc5d
ian <ianopolous@protonmail.com>
2022-05-06 09:03:26 +0100

    Release v0.9.2

--- src/peergos/server/UserService.java
+++ src/peergos/server/UserService.java
@@ -34,7 +34,7 @@ import java.util.concurrent.*;
 public class UserService {
 	private static final Logger LOG = Logging.LOG();
 
-    public static final Version CURRENT_VERSION = Version.parse("0.9.1");
+    public static final Version CURRENT_VERSION = Version.parse("0.9.2");
     public static final String UI_URL = "/";
 
     private static void initTLS() {
