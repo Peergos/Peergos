bd4b921ac6f20a6898e6e048af82b4fb0c03bf62
ian <ianopolous@protonmail.com>
2022-10-24 10:47:32 +0100

    Factor out pointer cache update logic

--- src/peergos/server/mutable/JdbcPointerCache.java
+++ src/peergos/server/mutable/JdbcPointerCache.java
@@ -21,26 +21,15 @@ public class JdbcPointerCache implements PointerCache {
     }
 
     @Override
-    public synchronized CompletableFuture<Boolean> put(PublicKeyHash owner, PublicKeyHash writer, byte[] writerSignedBtreeRootHash) {
+    public synchronized CompletableFuture<Boolean> put(PublicKeyHash owner, PublicKeyHash writer, byte[] signedUpdate) {
         return store.getPointer(writer)
-                .thenCompose(current -> {
-                    if (current.isPresent() && Arrays.equals(current.get(), writerSignedBtreeRootHash))
-                        return Futures.of(true);
-                    Optional<PointerUpdate> currentVal = current.map(CborObject::fromByteArray).map(PointerUpdate::fromCbor);
-
-                    return storage.getSigningKey(writer).thenCompose(signerOpt -> {
-                        if (signerOpt.isEmpty())
-                            throw new IllegalStateException("Couldn't retrieve signing key!");
-                        PointerUpdate newVal = PointerUpdate.fromCbor(CborObject.fromByteArray(signerOpt.get().unsignMessage(writerSignedBtreeRootHash)));
-
-                        if (currentVal.isPresent() && currentVal.get().sequence.isPresent()) {
-                            long currentSeq = currentVal.get().sequence.get();
-                            if (newVal.sequence.isEmpty() || newVal.sequence.get() < currentSeq)
-                                throw new IllegalStateException("Invalid pointer update! Sequence number must increase.");
-                        }
-                        return store.setPointer(writer, current, writerSignedBtreeRootHash);
-                    });
-                });
+                .thenCompose(current -> storage.getSigningKey(writer).thenCompose(signerOpt -> {
+                    if (signerOpt.isEmpty())
+                        throw new IllegalStateException("Couldn't retrieve signing key!");
+                    if (doUpdate(current, signedUpdate, signerOpt.get()))
+                        return store.setPointer(writer, current, signedUpdate);
+                    return Futures.of(false);
+                }));
     }
 
     @Override
