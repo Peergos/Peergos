df0f52f0086e5419f1fe4a827c2c75a4edf788a1
ian <ianopolous@protonmail.com>
2023-07-26 10:05:36 +0100

    Randomise test base port per JVM

--- src/peergos/server/tests/util/TestPorts.java
+++ src/peergos/server/tests/util/TestPorts.java
@@ -1,10 +1,11 @@
 package peergos.server.tests.util;
 
+import java.util.*;
 import java.util.concurrent.atomic.*;
 
 public class TestPorts {
 
-    private static final AtomicInteger port = new AtomicInteger(((int)System.currentTimeMillis()) % 50_000);
+    private static final AtomicInteger port = new AtomicInteger(new Random().nextInt(50_000));
     public static int getPort() {
         return 9050 + (port.incrementAndGet() % 50_000);
     }
