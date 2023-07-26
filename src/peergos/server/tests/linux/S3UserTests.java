67a956e15c80fd429ecaf7d7b4ec853cc465d5a2
ian <ianopolous@protonmail.com>
2023-07-26 13:17:16 +0100

    Use TestPorts.getPort in S3UserTests

--- src/peergos/server/tests/linux/S3UserTests.java
+++ src/peergos/server/tests/linux/S3UserTests.java
@@ -4,6 +4,7 @@ import org.junit.*;
 import peergos.server.*;
 import peergos.server.storage.*;
 import peergos.server.tests.*;
+import peergos.server.tests.util.*;
 import peergos.server.util.*;
 import peergos.shared.*;
 import peergos.shared.crypto.hash.*;
@@ -67,11 +68,11 @@ public class S3UserTests extends UserTests {
         services.add(pki);
 
         // start ipfs S3 node
-        int ipfsApiPort = 9000 + random.nextInt(8000);
-        int ipfsGatewayPort = 9000 + random.nextInt(8000);
-        int ipfsSwarmPort = 9000 + random.nextInt(8000);
-        int proxyTargetPort = 9000 + random.nextInt(8000);
-        int allowPort = 9000 + random.nextInt(8000);
+        int ipfsApiPort = TestPorts.getPort();
+        int ipfsGatewayPort = TestPorts.getPort();
+        int ipfsSwarmPort = TestPorts.getPort();
+        int proxyTargetPort = TestPorts.getPort();
+        int allowPort = TestPorts.getPort();
         Args ipfsArgs = withS3(buildArgs())
                 .with("useIPFS", "true")
                 .with("ipfs-api-address", "/ip4/127.0.0.1/tcp/" + ipfsApiPort)
@@ -85,7 +86,7 @@ public class S3UserTests extends UserTests {
         argsToCleanUp.add(ipfsArgs);
 
         // start direct S3 node
-        int peergosPort = 9000 + random.nextInt(8000);
+        int peergosPort = TestPorts.getPort();
         Cid ourId = new ContentAddressedStorage.HTTP(new JavaPoster(new URL("http://localhost:" + ipfsApiPort), false), false, crypto.hasher).id().get();
         Args peergosArgs = withS3(buildArgs())
                 .with("port", "" + peergosPort)
