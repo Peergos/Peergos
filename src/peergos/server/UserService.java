39fcb85c5b184c766fc982c1e1c986bb51dc0f41
ian <ianopolous@protonmail.com>
2023-08-03 14:14:11 +0100

    Bump version to v0.13.0

--- src/peergos/server/UserService.java
+++ src/peergos/server/UserService.java
@@ -34,7 +34,7 @@ import java.util.concurrent.*;
 public class UserService {
 	private static final Logger LOG = Logging.LOG();
 
-    public static final Version CURRENT_VERSION = Version.parse("0.12.0");
+    public static final Version CURRENT_VERSION = Version.parse("0.13.0");
     public static final String UI_URL = "/";
 
     private static void initTLS() {
