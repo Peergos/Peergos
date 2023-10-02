5e566ee080e3095880932d8051ee903b6b250b6d
ian <ianopolous@protonmail.com>
2023-10-02 09:33:58 +0100

    Try reducing netty thread count

--- src/peergos/server/Main.java
+++ src/peergos/server/Main.java
@@ -1,6 +1,7 @@
 package peergos.server;
 
 import com.webauthn4j.data.client.*;
+import io.netty.util.*;
 import peergos.server.cli.CLI;
 import peergos.server.login.*;
 import peergos.server.messages.*;
