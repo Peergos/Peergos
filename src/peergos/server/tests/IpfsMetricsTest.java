ed5a51ed8afd90eeced451b236e31adb10f220dc
ian <ianopolous@protonmail.com>
2023-10-30 23:20:07 +0000

    Fix test

--- src/peergos/server/tests/IpfsMetricsTest.java
+++ src/peergos/server/tests/IpfsMetricsTest.java
@@ -4,6 +4,8 @@ import org.junit.*;
 import peergos.server.storage.*;
 import peergos.server.util.*;
 
+import java.util.*;
+
 public class IpfsMetricsTest {
 
     @Test
@@ -11,8 +13,10 @@ public class IpfsMetricsTest {
         Args a = Args.parse(new String[]{"-collect-metrics", "true",
                 "-metrics.address", "192.168.10.1",
                 "-ipfs.metrics.port", "9000",
-                "-proxy-target", "/ip4/127.0.0.1/tcp/8000"
-        });
+                "-proxy-target", "/ip4/127.0.0.1/tcp/8000",
+                "-ipfs-api-address", "/ip4/127.0.0.1/tcp/5001",
+                "ipfs-gateway-address", "/ip4/127.0.0.1/tcp/8080",
+        }, Optional.empty(), false);
         IpfsWrapper.IpfsConfigParams conf = IpfsWrapper.buildConfig(a);
         Assert.assertTrue(conf.enableMetrics);
     }
