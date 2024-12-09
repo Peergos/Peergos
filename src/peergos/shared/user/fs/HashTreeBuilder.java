d74930262003b89058e7bbf6f8fe58447c7cc4f9
ian <ianopolous@protonmail.com>
2024-12-09 21:58:20 +0000

    Fix hashtree builder

--- src/peergos/shared/user/fs/HashTreeBuilder.java
+++ src/peergos/shared/user/fs/HashTreeBuilder.java
@@ -2,29 +2,30 @@ package peergos.shared.user.fs;
 
 import peergos.shared.crypto.hash.Hasher;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 
 public class HashTreeBuilder {
 
-    private final List<byte[]> chunkHashes;
+    private final byte[][] chunkHashes;
 
     public HashTreeBuilder(long filesize) {
-        this.chunkHashes = new ArrayList<>( (int)(filesize/ Chunk.MAX_SIZE));
+        this.chunkHashes = new byte[filesize == 0 ? 1 : ((int)((filesize + Chunk.MAX_SIZE - 1) / Chunk.MAX_SIZE))][];
     }
 
     public CompletableFuture<Boolean> setChunk(int chunkIndex, byte[] chunk, Hasher h) {
         return h.sha256(chunk)
-                .thenApply(hash -> chunkHashes.set(chunkIndex, hash))
-                .thenApply(x -> true);
+                .thenApply(hash -> {
+                    chunkHashes[chunkIndex] = hash;
+                    return true;
+                });
     }
 
     public void setChunkHash(int chunkIndex, byte[] hash) {
-        chunkHashes.set(chunkIndex, hash);
+        chunkHashes[chunkIndex] = hash;
     }
 
     public CompletableFuture<HashTree> complete(Hasher h) {
-        return HashTree.build(chunkHashes, h);
+        return HashTree.build(Arrays.asList(chunkHashes), h);
     }
 }
