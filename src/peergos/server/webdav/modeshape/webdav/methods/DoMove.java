163a870ee2e85e928dc418cc824c3aa49ad4b7e7
ian <ianopolous@protonmail.com>
2024-12-11 14:31:40 +0000

    Add webdav checks and errors around move

--- src/peergos/server/webdav/modeshape/webdav/methods/DoMove.java
+++ src/peergos/server/webdav/modeshape/webdav/methods/DoMove.java
@@ -17,6 +17,7 @@ package peergos.server.webdav.modeshape.webdav.methods;
 
 import peergos.server.webdav.modeshape.webdav.ITransaction;
 import peergos.server.webdav.modeshape.webdav.IWebdavStore;
+import peergos.server.webdav.modeshape.webdav.StoredObject;
 import peergos.server.webdav.modeshape.webdav.WebdavStatus;
 import peergos.server.webdav.modeshape.webdav.exceptions.AccessDeniedException;
 import peergos.server.webdav.modeshape.webdav.exceptions.LockFailedException;
@@ -26,6 +27,8 @@ import peergos.server.webdav.modeshape.webdav.locking.ResourceLocks;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import peergos.shared.util.PathUtil;
+
 import java.io.IOException;
 import java.util.Hashtable;
 
@@ -33,13 +36,16 @@ public class DoMove extends AbstractMethod {
 
     private final ResourceLocks resourceLocks;
     private final IWebdavStore store;
+    private final DoDelete doDelete;
     private final boolean readOnly;
 
     public DoMove( ResourceLocks resourceLocks,
                    IWebdavStore store,
+                   DoDelete doDelete,
                    boolean readOnly ) {
         this.resourceLocks = resourceLocks;
         this.store = store;
+        this.doDelete = doDelete;
         this.readOnly = readOnly;
     }
 
@@ -70,10 +76,61 @@ public class DoMove extends AbstractMethod {
                 return;
             }
 
+            if (sourcePath.equals(destinationPath)) {
+                resp.sendError(WebdavStatus.SC_FORBIDDEN);
+                return;
+            }
+
+            if (!isUnlocked(transaction, req, resourceLocks, PathUtil.get(destinationPath).getParent().toString())) {
+                resp.setStatus(WebdavStatus.SC_LOCKED);
+                return; // parentDestination is locked
+            }
+
+            if (!isUnlocked(transaction, req, resourceLocks, destinationPath)) {
+                resp.setStatus(WebdavStatus.SC_LOCKED);
+                return; // destination is locked
+            }
+            boolean overwrite = shouldOverwrite(req);
+
             String tempLockOwner = "doMove" + System.currentTimeMillis() + req.toString();
 
             if (resourceLocks.lock(transaction, sourcePath, tempLockOwner, false, 0, TEMP_TIMEOUT, TEMPORARY)) {
                 try {
+                    StoredObject sourceSo = store.getStoredObject(transaction, sourcePath);
+                    // Retrieve the resources
+                    if (sourceSo == null) {
+                        resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                        return;
+                    }
+
+                    if (sourceSo.isNullResource()) {
+                        String methodsAllowed = DeterminableMethod.determineMethodsAllowed(sourceSo);
+                        resp.addHeader("Allow", methodsAllowed);
+                        resp.sendError(WebdavStatus.SC_METHOD_NOT_ALLOWED);
+                        return;
+                    }
+
+                    errorList = new Hashtable<String, Integer>();
+
+                    StoredObject destinationSo = store.getStoredObject(transaction, destinationPath);
+
+                    if (overwrite) {
+
+                        // Delete destination resource, if it exists
+                        if (destinationSo != null) {
+                            doDelete.deleteResource(transaction, destinationPath, errorList, req, resp);
+                        } else {
+                            resp.setStatus(WebdavStatus.SC_CREATED);
+                        }
+                    } else {
+                        // If the destination exists, then it's a conflict
+                        if (destinationSo != null) {
+                            resp.sendError(WebdavStatus.SC_PRECONDITION_FAILED);
+                            return;
+                        }
+                        resp.setStatus(WebdavStatus.SC_CREATED);
+
+                    }
                     store.moveResource(transaction, sourcePath, destinationPath);
 
                 } catch (AccessDeniedException e) {
@@ -93,5 +150,15 @@ public class DoMove extends AbstractMethod {
             resp.sendError(WebdavStatus.SC_FORBIDDEN);
 
         }
+    }
+
+    private boolean shouldOverwrite( HttpServletRequest req ) {
+        boolean overwrite = true;
+        String overwriteHeader = req.getHeader("Overwrite");
+
+        if (overwriteHeader != null) {
+            overwrite = overwriteHeader.equalsIgnoreCase("T");
+        }
+        return overwrite;
     }
 }
