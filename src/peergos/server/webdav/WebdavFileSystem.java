4331e4f051c6072df40204254ac47c18b82d1138
ian <ianopolous@protonmail.com>
2025-01-01 16:06:26 +1100

    Allow rendering global root in webdav

--- src/peergos/server/webdav/WebdavFileSystem.java
+++ src/peergos/server/webdav/WebdavFileSystem.java
@@ -272,7 +272,13 @@ public class WebdavFileSystem implements IWebdavStore {
         StoredObject so = null;
         Path path = new File(uri).toPath();
         Optional<FileWrapper> fwOpt = context.getByPath(path.toString()).join();
-        if (fwOpt.isPresent() && !fwOpt.get().getFileProperties().isHidden) {
+        if (fwOpt.isPresent() && fwOpt.get().isRoot()) {
+            so = new StoredObject();
+            so.setFolder(true);
+            so.setLastModified(new Date(0));
+            so.setCreationDate(new Date(0));
+            so.setResourceLength(0);
+        } else if (fwOpt.isPresent() && !fwOpt.get().getFileProperties().isHidden) {
             so = new StoredObject();
             FileWrapper fw = fwOpt.get();
             so.setFolder(fw.isDirectory());
