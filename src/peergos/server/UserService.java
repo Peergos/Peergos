33a9db91bc49af10ebbe6097416404841816b510
ian <ianopolous@protonmail.com>
2025-06-30 11:20:07 +0100

    v1.7.1

--- src/peergos/server/UserService.java
+++ src/peergos/server/UserService.java
@@ -32,7 +32,7 @@ import java.security.cert.*;
 public class UserService {
 	private static final Logger LOG = Logging.LOG();
 
-    public static final Version CURRENT_VERSION = Version.parse("1.7.0");
+    public static final Version CURRENT_VERSION = Version.parse("1.7.1");
     public static final String UI_URL = "/";
 
     private static void initTLS() {
