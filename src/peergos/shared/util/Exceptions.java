9f5146b7333a5021487a5b9519557632eb0a2a72
ian <ianopolous@protonmail.com>
2026-01-22 15:49:34 +0000

    Fix S3 404 handling after netty switch

--- src/peergos/shared/util/Exceptions.java
+++ src/peergos/shared/util/Exceptions.java
@@ -5,6 +5,8 @@ import java.util.concurrent.*;
 public class Exceptions {
     public static Throwable getRootCause(Throwable t) {
         Throwable cause = t.getCause();
+        if (t instanceof CompletionException)
+            return getRootCause(cause);
         if (t instanceof ExecutionException)
             return getRootCause(cause);
         if (t instanceof RuntimeException && cause != null && cause != t)
