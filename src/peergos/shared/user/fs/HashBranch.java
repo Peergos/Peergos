f1fd295f9d111a3fde1f9e8461e3e013b34a9ffa
ian <ianopolous@protonmail.com>
2026-03-10 20:43:33 +0000

    Implement HashBranch.toString()

--- src/peergos/shared/user/fs/HashBranch.java
+++ src/peergos/shared/user/fs/HashBranch.java
@@ -48,6 +48,11 @@ public class HashBranch implements Cborable {
     }
 
     @Override
+    public String toString() {
+        return rootHash.toString();
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
