ce876342490f21cf7734e0b6453b178e046ffd4f
ian <ianopolous@protonmail.com>
2026-04-05 19:48:02 +0100

    Add util for checking block presence in S3

--- src/peergos/server/storage/S3BlockStorage.java
+++ src/peergos/server/storage/S3BlockStorage.java
@@ -1675,6 +1675,70 @@ public class S3BlockStorage implements DeletableContentAddressedStorage {
         throw new IllegalStateException("Shouldn't get here.");
     }
 
+    public static class HasBlock {
+        public static void main(String[] args) {
+            Args a = Args.parse(args);
+            Logging.init(a.with("log-to-console", "true"));
+            Crypto crypto = Main.initCrypto();
+            Hasher hasher = crypto.hasher;
+            S3Config config = S3Config.build(a, Optional.empty());
+            boolean versioned = a.getBoolean("s3.versioned-bucket");
+            boolean usePostgres = a.getBoolean("use-postgres", false);
+            SqlSupplier sqlCommands = usePostgres ?
+                    new PostgresCommands() :
+                    new SqliteCommands();
+            Supplier<Connection> database = Main.getDBConnector(a, "mutable-pointers-file");
+            Supplier<Connection> transactionsDb = Main.getDBConnector(a, "transactions-sql-file");
+            TransactionStore transactions = JdbcTransactionStore.build(transactionsDb, sqlCommands);
+            BlockRequestAuthoriser authoriser = (c, b, s, auth) -> Futures.of(true);
+            BlockMetadataStore meta = Builder.buildBlockMetadata(a);
+            Supplier<Connection> usageDb = Main.getDBConnector(a, "space-usage-sql-file");
+            UsageStore usage = new JdbcUsageStore(usageDb, sqlCommands);
+            Supplier<Connection> statusDb = Main.getDBConnector(a, "partition-status-file");
+            PartitionStatus partitioned = new JdbcPartitionStatus(statusDb, sqlCommands);
+            JavaPoster p2pHttpProxy = Builder.buildP2pHttpProxy(a);
+            ContentAddressedStorageProxy p2pHttpFallback = new ContentAddressedStorageProxy.HTTP(p2pHttpProxy);
+            p2pHttpFallback = Builder.buildP2PBlockRetrieverForS3(a, usage, hasher, p2pHttpFallback);
+            S3BlockStorage s3 = new S3BlockStorage(config, List.of(Cid.decode(a.getArg("ipfs.id"))),
+                    BlockStoreProperties.empty(), "localhost:8000", transactions, authoriser, null, meta, usage,
+                    new RamBlockCache(1024, 100),
+                    new FileBlockBuffer(a.fromPeergosDir("s3-block-buffer-dir", "block-buffer"), usage),
+                    Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE,
+                    versioned, a.getPeergosDir(), partitioned, hasher,
+                    new RAMStorage(hasher), p2pHttpFallback);
+            JdbcIpnsAndSocial rawPointers = new JdbcIpnsAndSocial(database, sqlCommands);
+
+            MutablePointers localPointers = UserRepository.build(s3, rawPointers, hasher);
+            Multihash pkiServerNodeId = Builder.getPkiServerId(a);
+            MutablePointersProxy proxingMutable = new HttpMutablePointers(p2pHttpProxy, pkiServerNodeId);
+            LinkRetrievalCounter linkCounts = new JdbcLinkRetrievalcounter(Main.getDBConnector(a, "link-counts-sql-file", database), sqlCommands);
+            JdbcIpnsAndSocial rawSocial = new JdbcIpnsAndSocial(Builder.getDBConnector(a, "social-sql-file", database), sqlCommands);
+            String listeningHost = a.getArg(Main.LISTEN_HOST.name, "localhost");
+            int webPort = a.getInt("port");
+            Optional<String> tlsHostname = a.hasArg("tls.keyfile.password") ? Optional.of(listeningHost) : Optional.empty();
+            Optional<String> publicHostname = tlsHostname.isPresent() ? tlsHostname : a.getOptionalArg("public-domain");
+            Origin origin = new Origin(publicHostname.map(host -> (Main.isLanIP(host) ? "http://" : "https://") + host).orElse("http://localhost:" + webPort));
+            String rpId = publicHostname.orElse("localhost");
+            JdbcAccount rawAccount = new JdbcAccount(Builder.getDBConnector(a, "account-sql-file", database), sqlCommands, origin, rpId);
+            Account account = new AccountWithStorage(s3, localPointers, rawAccount);
+            boolean isPki = false;
+            InetSocketAddress userAPIAddress = new InetSocketAddress(listeningHost, webPort);
+            boolean localhostApi = userAPIAddress.getHostName().equals("localhost");
+            QuotaAdmin userQuotas = Main.buildSpaceQuotas(a, s3,
+                    Main.getDBConnector(a, "space-requests-sql-file", database),
+                    Main.getDBConnector(a, "quotas-sql-file", database), isPki, localhostApi);
+            JdbcBatCave batStore = new JdbcBatCave(Main.getDBConnector(a, "bat-store", database), sqlCommands);
+
+            CoreNode core = Builder.buildCorenode(a, s3, transactions, rawPointers, localPointers, proxingMutable,
+                    rawSocial, usage, userQuotas, rawAccount, batStore, account, linkCounts, crypto);
+
+            s3.setPki(core);
+            PublicKeyHash owner = PublicKeyHash.fromString(a.getArg("owner"));
+            Cid hash = Cid.decode(a.getArg("hash"));
+            System.out.println("Block present " + owner  + ": " + hash + " " + s3.hasBlock(owner, hash));
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Args a = Args.parse(args);
         Logging.init(a.with("log-to-console", "true"));
