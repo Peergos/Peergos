61e3106d9a28c6b17ac0e0ad04ae047fbe594cf2
ian <ianopolous@protonmail.com>
2026-05-15 09:22:09 +0100

    Handle failure loading native thumbnailer

--- src/peergos/server/util/JvmThumbnailer.java
+++ src/peergos/server/util/JvmThumbnailer.java
@@ -6,12 +6,17 @@ import peergos.shared.user.fs.ThumbnailGenerator;
 public class JvmThumbnailer {
 
     public static void initJava() {
-        FFmpegThumbnailer.create().ifPresentOrElse(
-                ffmpeg -> {
-                    ThumbnailGenerator.setInstance(ffmpeg);
-                    ThumbnailGenerator.setVideoInstance(ffmpeg);
-                },
-                () -> ThumbnailGenerator.setInstance(new JavaImageThumbnailer())
-        );
+        try {
+            FFmpegThumbnailer.create().ifPresentOrElse(
+                    ffmpeg -> {
+                        ThumbnailGenerator.setInstance(ffmpeg);
+                        ThumbnailGenerator.setVideoInstance(ffmpeg);
+                    },
+                    () -> ThumbnailGenerator.setInstance(new JavaImageThumbnailer())
+            );
+        } catch (Throwable e) {
+            System.err.println("Unable to load native thumbnailer: " + e.getMessage());
+            ThumbnailGenerator.setInstance(new JavaImageThumbnailer());
+        }
     }
 }
