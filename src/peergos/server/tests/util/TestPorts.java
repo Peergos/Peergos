8f9791a49dc1059a94f18d2a6f8703a82c415a7a
ian <ianopolous@protonmail.com>
2026-06-05 16:07:33 +0100

    Synchronize TestPorts

--- src/peergos/server/tests/util/TestPorts.java
+++ src/peergos/server/tests/util/TestPorts.java
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.*;
 public class TestPorts {
 
     private static final AtomicInteger port = new AtomicInteger(new Random().nextInt(50_000));
-    public static int getPort() {
+    public static synchronized int getPort() {
         return 9050 + (port.incrementAndGet() % 50_000);
     }
 }
